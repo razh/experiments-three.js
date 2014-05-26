96f32b491b8172173a027a79beaebd25c5403c55
Raymond Zhou <somecrazyidiot@gmail.com>
2014-05-26 02:44:20 -0400

    ik: Add plane-intersection interaction.

--- ik/js/main.js
+++ ik/js/main.js
@@ -5,7 +5,10 @@
   var container;
 
   var scene, camera, renderer;
+  var projector;
+
   var geometry, material, plane;
+  var sphereGeometry, sphereMaterial, sphere;
 
   function init() {
     container = document.createElement( 'div' );
@@ -17,14 +20,31 @@
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+
+    camera.position.set( 0, -400, 200 );
+    camera.rotation.x = THREE.Math.degToRad( 60 );
+
     scene.add( camera );
 
-    geometry = new THREE.PlaneGeometry( 1000, 1000, 20, 20 );
+    // Projector.
+    projector = new THREE.Projector();
+
+    // Plane.
+    geometry = new THREE.PlaneGeometry( 500, 500, 20, 20 );
     material = new THREE.MeshBasicMaterial({
       wireframe: true
     });
     plane = new THREE.Mesh( geometry, material );
     scene.add( plane );
+
+    // Sphere.
+    sphereGeometry = new THREE.SphereGeometry( 10 );
+    sphereMaterial = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      wireframe: true
+    });
+    sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+    scene.add( sphere );
   }
 
   function animate() {
@@ -34,5 +54,36 @@
 
   init();
   animate();
+
+  window.addEventListener( 'mousemove', function( event ) {
+    // Calculate intersection.
+    var vector = new THREE.Vector3(
+      ( event.pageX / window.innerWidth ) * 2 - 1,
+      -( event.pageY / window.innerHeight ) * 2 + 1,
+      0
+    );
+
+    projector.unprojectVector( vector, camera );
+
+    var raycaster = new THREE.Raycaster(
+      camera.position,
+      vector.sub( camera.position ).normalize()
+    );
+
+    var intersections = raycaster.intersectObject( plane );
+    if ( intersections[0] ) {
+      sphere.position.copy( intersections[0].point );
+    }
+  });
+
+  window.addEventListener( 'wheel', function( event ) {
+    if ( !event.deltaY ) {
+      return;
+    }
+
+    event.preventDefault();
+    plane.position.z += event.deltaY;
+    sphere.position.z = plane.position.z;
+  });
 
 }) ( window, document );
