15eaa41deb56e3c61f1135e22c820431645fc1e7
Raymond Zhou <somecrazyidiot@gmail.com>
2014-05-26 15:14:31 -0400

    ik: Add FABRIK algorithm.

--- ik/js/main.js
+++ ik/js/main.js
@@ -7,21 +7,101 @@
   var scene, camera, renderer;
   var projector;
 
-  var geometry, material, plane;
+  var planeGeometry, planeMaterial, plane;
   var sphereGeometry, sphereMaterial, sphere;
 
   var ikGeometry, ikMaterial, ik;
+  var ikLengths = [ 80, 30, 50, 70, 40 ];
 
-  function ikGeometryFromArray( initialValue, lengths ) {
+  var EPSILON = 1e-2;
+
+  function ikGeometryFromArray( lengths ) {
     var geometry = new THREE.Geometry();
 
-    // Creates a vertical line point down from the origin.
+    // Creates a vertical line pointing up from its local origin.
+    geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
     lengths.reduce(function( z, length ) {
+      z -= length;
       geometry.vertices.push( new THREE.Vector3( 0, 0, z ) );
-      return z + length;
-    }, initialValue || 0 );
+      return z;
+    }, 0 );
 
     return geometry;
+  }
+
+  function ikSet( point, line, lengths ) {
+    point = new THREE.Vector3().copy( point );
+
+    var geometry = line.geometry;
+    var distance = line.position.distanceTo( point );
+
+    var totalLength = lengths.reduce(function( sum, length ) {
+      return sum + length;
+    });
+
+    // Enter line coordinate space.
+    point.sub( line.position );
+
+    var vertices = geometry.vertices;
+    var count = vertices.length;
+    var temp = new THREE.Vector3();
+    var vi, vj, vf;
+    var di, dj, df;
+    var t;
+    var i, il;
+    // Not reachable.
+    if ( distance > totalLength ) {
+      for ( i = 0, il = count - 1; i < il; i++ ) {
+        vi = vertices[i];
+        vj = vertices[ i + 1 ];
+
+        di = vi.distanceTo( point );
+        t = lengths[i] / di;
+
+        temp.copy( vi ).lerp( point, t );
+        vj.copy( temp );
+      }
+    }
+    // Reachable.
+    else {
+      vf = vertices[ count - 1 ];
+      df = vf.distanceTo( point );
+
+      while ( df > EPSILON ) {
+        vf.copy( point );
+
+        // Stage 1: Forward reaching.
+        for ( i = count - 2; i >= 0; i-- ) {
+          vi = vertices[i];
+          vj = vertices[ i + 1 ];
+
+          dj = vi.distanceTo( vj );
+          t = lengths[i] / dj;
+
+          temp.copy( vj ).lerp( vi, t );
+          vi.copy( temp );
+        }
+
+        vi = vertices[0];
+        vi.set( 0, 0, 0 );
+
+        // Stage 2: Backward reaching.
+        for ( i = 0; i < count - 1; i++ ) {
+          vi = vertices[i];
+          vj = vertices[ i + 1 ];
+
+          dj = vi.distanceTo( vj );
+          t = lengths[i] / dj;
+
+          temp.copy( vi ).lerp( vj, t );
+          vj.copy( temp );
+        }
+
+        df = vf.distanceTo( point );
+      }
+    }
+
+    ik.geometry.verticesNeedUpdate = true;
   }
 
   function init() {
@@ -44,11 +124,11 @@
     projector = new THREE.Projector();
 
     // Plane.
-    geometry = new THREE.PlaneGeometry( 500, 500, 20, 20 );
-    material = new THREE.MeshBasicMaterial({
+    planeGeometry = new THREE.PlaneGeometry( 500, 500, 20, 20 );
+    planeMaterial = new THREE.MeshBasicMaterial({
       wireframe: true
     });
-    plane = new THREE.Mesh( geometry, material );
+    plane = new THREE.Mesh( planeGeometry, planeMaterial );
     scene.add( plane );
 
     // Sphere.
@@ -61,12 +141,13 @@
     scene.add( sphere );
 
     // IK.
-    ikGeometry = ikGeometryFromArray( -100, [ 80, 30, 50, 70, 40 ] );
+    ikGeometry = ikGeometryFromArray( ikLengths );
     ikMaterial = new THREE.LineBasicMaterial({
       color: '#f43',
       linewidth: 5
     });
     ik = new THREE.Line( ikGeometry, ikMaterial );
+    ik.position.z = 100;
     scene.add( ik );
   }
 
@@ -96,6 +177,7 @@
     var intersections = raycaster.intersectObject( plane );
     if ( intersections[0] ) {
       sphere.position.copy( intersections[0].point );
+      ikSet( sphere.position, ik, ikLengths );
     }
   });
 
@@ -107,6 +189,7 @@
     event.preventDefault();
     plane.position.z += event.deltaY;
     sphere.position.z = plane.position.z;
+    ikSet( sphere.position, ik, ikLengths );
   });
 
 }) ( window, document );
