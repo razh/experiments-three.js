fb449a8f5709c67e6f00032e4c9d1aac32c054c4
Raymond Zhou <somecrazyidiot@gmail.com>
2014-07-10 02:07:43 -0400

    manifolds: Add phase factor function.
    Fix incorrect naming of cosine function with imaginary component.
    
    Use Math.sinh/cosh if available.

--- manifolds/js/main.js
+++ manifolds/js/main.js
@@ -2,19 +2,21 @@
 (function( window, document, undefined ) {
   'use strict';
 
+  var TAU = 2 * Math.PI;
+
   var container;
 
   var scene, camera, renderer;
 
   // Hyperbolic trigonometric functions.
   // Cached Math.exp(x) is significantly less accurate.
-  function cosh( x ) {
+  var cosh = Math.cosh || function cosh( x ) {
     return ( Math.exp( x ) + Math.exp( -x ) ) / 2;
-  }
+  };
 
-  function sinh( x ){
+  var sinh = Math.sinh || function sinh( x ){
     return ( Math.exp( x ) - Math.exp( -x ) ) / 2;
-  }
+  };
 
   // Trigonometric functions with complex arguments.
   function sini( real, imag ) {
@@ -24,10 +26,31 @@
     };
   }
 
-  function sini( real, imag ) {
+  function cosi( real, imag ) {
     return {
       real:  Math.cos( real ) * cosh( imag ),
       imag: -Math.sin( real ) * sinh( imag )
+    };
+  }
+
+  /**
+   * The phase factor function s(k, n) is defined as:
+   *
+   *   s(k, n) = exp(2PIik / n)
+   *
+   * We use Euler's formula to calculate the complex exponential function:
+   *
+   *   e^ix = cos(x) + i * sin(x)
+   *
+   * This phase factor represents the nth root of unity for integers
+   * 0 <= k <= n - 1.
+   */
+  function phaseFactor( k, n ) {
+    var x = ( TAU * k ) / n;
+
+    return {
+      real: Math.cos( x ),
+      imag: Math.sin( x )
     };
   }
 
