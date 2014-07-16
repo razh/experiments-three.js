890744d358292b7ffa840bf4d6347b7dd0a1f5d9
Raymond Zhou <somecrazyidiot@gmail.com>
2014-07-15 22:55:38 -0400

    manifolds: Add resize event listener.

--- manifolds/js/main.js
+++ manifolds/js/main.js
@@ -312,4 +312,11 @@
   init();
   animate();
 
+  window.addEventListener( 'resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  });
+
 }) ( window, document );
