3b9738e5eec84e10d5bd9874d1edcec15f5d2e62
Raymond Zhou <somecrazyidiot@gmail.com>
2014-09-24 00:20:13 -0400

    audio: Add positional audio.

--- audio/js/main.js
+++ audio/js/main.js
@@ -7,12 +7,18 @@
   var scene, camera, controls, renderer;
 
   var planeGeometry, planeMaterial, planeMesh;
+  var sourceGeometry, sourceMaterial, sourceMesh;
+
+  // Front direction vector used for AudioListener orientation.
+  var front = new THREE.Vector3();
 
   var AudioContext = window.AudioContext || window.webkitAudioContext;
   var audioCtx;
 
+  var listener, panner;
+
   var osc, lfo, lfoGain, gain;
-  var playing = true;
+  var playing = false;
 
   var A4 = 69;
   var C3 = 48;
@@ -35,7 +41,7 @@
     scene.add( camera );
 
     controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.maxPolarAngle = Math.PI / 2 - Math.PI / 16;
+    controls.maxPolarAngle = Math.PI / 2 + Math.PI / 16;
 
     planeGeometry = new THREE.PlaneGeometry( 32, 32 );
     planeMaterial = new THREE.MeshBasicMaterial({ color: 0x333333 });
@@ -43,10 +49,20 @@
     planeMesh.rotation.x = -Math.PI / 2;
     scene.add( planeMesh );
 
-    controls.target.copy( planeMesh.position );
+    // Sound source mesh.
+    sourceGeometry = new THREE.SphereGeometry( 1 );
+    sourceMaterial = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x555555 });
+    sourceMesh = new THREE.Mesh( sourceGeometry, sourceMaterial );
+    sourceMesh.position.y = 1;
+    scene.add( sourceMesh );
+
+    controls.target.copy( sourceMesh.position );
 
     // Initialize audio.
     audioCtx = new AudioContext();
+
+    listener = audioCtx.listener;
+    panner = audioCtx.createPanner();
 
     osc = audioCtx.createOscillator();
     osc.type = 'sine';
@@ -59,11 +75,12 @@
     lfoGain = audioCtx.createGain();
 
     gain = audioCtx.createGain();
-    gain.gain.value = 0.05;
+    gain.gain.value = 0;
 
     osc.connect( lfoGain );
     lfo.connect( lfoGain.gain );
-    lfoGain.connect( gain );
+    lfoGain.connect( panner );
+    panner.connect( gain );
     gain.connect( audioCtx.destination );
 
     osc.start();
@@ -71,6 +88,24 @@
   }
 
   function animate() {
+    listener.setPosition(
+      camera.position.x,
+      camera.position.y,
+      camera.position.z
+    );
+
+    front.subVectors( controls.target, camera.position )
+      .normalize();
+
+    listener.setOrientation(
+      front.x,
+      front.y,
+      front.z,
+      camera.up.x,
+      camera.up.y,
+      camera.up.z
+    );
+
     renderer.render( scene, camera );
     requestAnimationFrame( animate );
   }
@@ -84,10 +119,9 @@
       playing = !playing;
 
       if ( playing ) {
-        gain.gain.setTargetAtTime( 0.05, audioCtx.currentTime, 1 );
+        gain.gain.setTargetAtTime( 0.2, audioCtx.currentTime, 1 );
       } else {
         gain.gain.setTargetAtTime( 0, audioCtx.currentTime, 1 );
-
       }
     }
   });
