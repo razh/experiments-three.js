4b68fce2a37a931109e26065cd6de49a8aca71fa
Raymond Zhou <somecrazyidiot@gmail.com>
2014-10-20 23:44:59 -0400

    brdf[approx]: Add three.js boilerplate.

--- brdf/js/approx.js
+++ brdf/js/approx.js
@@ -1,3 +1,43 @@
+/*global THREE, requestAnimationFrame*/
 (function() {
   'use strict';
+
+  var container;
+
+  var scene, camera, controls, renderer;
+
+  var mesh;
+
+  function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
+    camera.position.set( 0, 0, 8 );
+    scene.add( camera );
+
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+    mesh = new THREE.Mesh(
+      new THREE.BoxGeometry( 1, 1, 1 ),
+      new THREE.MeshBasicMaterial({ color: '#fff' })
+    );
+
+    scene.add( mesh );
+  }
+
+  function animate() {
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+  }
+
+  init();
+  animate();
+
 }) ();
