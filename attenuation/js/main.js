b77a16534e28e2e38529867121c1184a0b5bde54
Raymond Zhou <somecrazyidiot@gmail.com>
2014-11-01 16:02:13 -0400

    attenuation: Add AreaLight dimension and attenuation controls.

--- attenuation/js/main.js
+++ attenuation/js/main.js
@@ -1,4 +1,4 @@
-/*global THREE, requestAnimationFrame*/
+/*global THREE, requestAnimationFrame, dat*/
 (function() {
   'use strict';
 
@@ -17,25 +17,35 @@
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
-    camera.position.set( 0, 0, 2 );
+    camera.position.set( 0, 0, 6 );
     scene.add( camera );
 
     controls = new THREE.OrbitControls( camera, renderer.domElement );
 
+    var material = new THREE.MeshPhongMaterial({
+      color: '#777',
+      specular: '#fff',
+    });
+
     var mesh = new THREE.Mesh(
-      new THREE.PlaneBufferGeometry( 2, 2 ),
-      new THREE.MeshPhongMaterial({
-        color: '#777',
-        specular: '#fff'
-      })
+      new THREE.IcosahedronGeometry( 2, 4 ),
+      material
     );
 
     scene.add( mesh );
 
     var areaLight = new THREE.AreaLight( 0xffffff );
-    areaLight.position.set( 0, 0, 2 );
+    areaLight.position.set( 0, 0, 6 );
     areaLight.normal.subVectors( mesh.position, areaLight.position );
     scene.add( areaLight );
+
+    var gui = new dat.GUI({ width: 320 });
+
+    gui.add( areaLight, 'width', 1, 8 );
+    gui.add( areaLight, 'height', 1, 8 );
+    gui.add( areaLight, 'constantAttenuation', 0, 2 );
+    gui.add( areaLight, 'linearAttenuation', 0, 2 );
+    gui.add( areaLight, 'quadraticAttenuation', 0, 2 );
   }
 
   function animate() {
