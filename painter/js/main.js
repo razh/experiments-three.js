b245b224f3168a8894916a7e18d0138aae0b7cef
Raymond Zhou <somecrazyidiot@gmail.com>
2014-12-25 00:28:23 -0500

    painter: Add initial boilerplate.

---
+++ painter/js/main.js
@@ -0,0 +1,31 @@
+/*global THREE, requestAnimationFrame*/
+(function() {
+  'use strict';
+
+  var container;
+
+  var scene, camera, renderer;
+
+  function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
+    scene.add( camera );
+  }
+
+  function animate() {
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+  }
+
+  init();
+  animate();
+
+}) ();
