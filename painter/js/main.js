b2509a5e765e8c2caa5cd0e28c63d2e677bd0eda
Raymond Zhou <somecrazyidiot@gmail.com>
2015-03-29 01:07:01 -0400

    painter: Add backing canvas height map.

--- painter/js/main.js
+++ painter/js/main.js
@@ -4,6 +4,13 @@
 
   var container;
 
+  var canvas, ctx;
+
+  var brushCanvas, brushCtx;
+  var brushGradient;
+  var brushRadius = 16;
+  var brushDiameter = 2 * brushRadius;
+
   var scene, camera, renderer;
   var raycaster;
   var mouse;
@@ -12,6 +19,8 @@
   var intersectionMesh;
 
   var mouseDown = false;
+
+  var size = 256;
 
   function init() {
     container = document.createElement( 'div' );
@@ -25,17 +34,17 @@
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
-    camera.position.set( 0, 0, 1 );
+    camera.position.set( 0, -0.75, 0.75 );
+    camera.lookAt( new THREE.Vector3() );
     scene.add( camera );
 
     raycaster = new THREE.Raycaster();
     mouse = new THREE.Vector2();
 
-    geometry = new THREE.PlaneGeometry( 1, 1 );
+    geometry = new THREE.PlaneBufferGeometry( 1, 1 );
     material = new THREE.MeshPhongMaterial();
 
     mesh = new THREE.Mesh( geometry, material );
-    mesh.rotation.x = -Math.PI / 4;
     scene.add( mesh );
 
     intersectionMesh = new THREE.Mesh(
@@ -45,9 +54,39 @@
     intersectionMesh.visible = false;
     scene.add( intersectionMesh );
 
-    var light = new THREE.PointLight( '#fff', 0.25 );
-    light.position.set( 1, 4, 0 );
+    var light = new THREE.PointLight( '#fff', 0.5 );
+    light.position.set( 1, 2, 1 );
     scene.add( light );
+
+    // Height map.
+    canvas = document.createElement( 'canvas' );
+    ctx    = canvas.getContext( '2d' );
+
+    canvas.width = size;
+    canvas.height = size;
+
+    canvas.style.position = 'fixed';
+    canvas.style.left = 0;
+    canvas.style.top = 0;
+
+    document.body.appendChild( canvas );
+
+    // Brush gradient.
+    brushCanvas = document.createElement( 'canvas' );
+    brushCtx    = brushCanvas.getContext( '2d' );
+
+    brushCanvas.width  = brushDiameter;
+    brushCanvas.height = brushDiameter;
+
+    brushGradient = ctx.createRadialGradient(
+      brushRadius, brushRadius, 0,
+      brushRadius, brushRadius, brushRadius
+    );
+    brushGradient.addColorStop( 0, '#222' );
+    brushGradient.addColorStop( 1, 'transparent' );
+
+    brushCtx.fillStyle = brushGradient;
+    brushCtx.fillRect( 0, 0, brushDiameter, brushDiameter );
   }
 
   function render() {
@@ -69,8 +108,18 @@
     intersectionMesh.visible = false;
     if ( intersections.length ) {
       var intersection = intersections[0];
-      intersectionMesh.position.copy( intersection.point );
+      var point = intersection.point;
+      intersectionMesh.position.copy( point );
       intersectionMesh.visible = true;
+
+      if ( mouseDown ) {
+        ctx.globalCompositeOperation = 'lighter';
+        ctx.drawImage(
+          brushCanvas,
+          size * (  point.x + 0.5 ) - brushRadius,
+          size * ( -point.y + 0.5 ) - brushRadius
+        );
+      }
     }
 
     render();
