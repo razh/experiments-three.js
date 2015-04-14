dcb39b081c4c998dfc6c9b081e6b2361e69167e8
Raymond Zhou <somecrazyidiot@gmail.com>
2015-04-13 22:14:38 -0400

    skeleton: Add resize event handler.

--- skeleton/js/main.js
+++ skeleton/js/main.js
@@ -165,4 +165,11 @@
   init();
   animate();
 
+  window.addEventListener( 'resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  });
+
 })();
