e8b2df20cd7a8f8d3c7da681e0e61d964e11dcc3
Raymond Zhou <somecrazyidiot@gmail.com>
2015-04-21 01:12:11 -0400

    tactics[agents]: Add Agent class stub.

---
+++ tactics/js/agents/agents.js
@@ -0,0 +1,18 @@
+/*global THREE*/
+/*exported Agent*/
+var Agent = (function() {
+  'use strict';
+
+  var geometry = new THREE.SphereGeometry( 1, 1 );
+  var material = new THREE.MeshBasicMaterial();
+
+  function Agent() {
+    THREE.Mesh.call( this, geometry, material );
+  }
+
+  Agent.prototype = Object.create( THREE.Mesh.prototype );
+  Agent.prototype.constructor = Agent;
+
+  return Agent;
+
+})();
