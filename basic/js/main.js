65c68523919a35170ef5193bc4c95e50d8760696
Raymond Zhou <somecrazyidiot@gmail.com>
2015-06-05 10:04:06 -0400

    basic: Add animated sphere geometry example.

---
+++ basic/js/main.js
@@ -0,0 +1,116 @@
+/*global THREE, dat, createAnimatedSphereGeometry*/
+(function() {
+  'use strict';
+
+  var container;
+
+  var scene, camera, controls, renderer;
+
+  var animateGeometry, geometry, mesh;
+  var light;
+
+  var clock;
+
+  var types = [ 'point', 'wireframe', 'mesh' ];
+
+  var constructors = {
+    point: THREE.PointCloud,
+    wireframe: THREE.Mesh,
+    mesh: THREE.Mesh
+  };
+
+  var materials = {
+    point: new THREE.PointCloudMaterial({
+      size: 0.01
+    }),
+
+    wireframe: new THREE.MeshPhongMaterial({
+      wireframe: true
+    }),
+
+    mesh: new THREE.MeshPhongMaterial({
+      shading: THREE.FlatShading
+    })
+  };
+
+  var config = {
+    count: 24,
+    type: 'point'
+  };
+
+  function createMesh() {
+    scene.remove( mesh );
+    mesh = new constructors[ config.type ]( geometry, materials[ config.type ] );
+    scene.add( mesh );
+  }
+
+  function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
+    camera.position.set( 0, 0, 2 );
+
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+    light = new THREE.HemisphereLight( '#fff', '#111' );
+    light.position.set( 8, 8, 0 );
+    scene.add( light );
+
+    clock = new THREE.Clock();
+
+    animateGeometry = createAnimatedSphereGeometry( 1, config.count, config.count );
+    geometry = animateGeometry( 1, 1 );
+
+    createMesh();
+
+    var gui = new dat.GUI();
+
+    gui.add( config, 'count', 3, 48 )
+      .step( 1 )
+      .listen()
+      .onChange(function() {
+        animateGeometry = createAnimatedSphereGeometry( 1, config.count, config.count );
+      });
+
+    gui.add( config, 'type', types )
+      .listen()
+      .onChange( createMesh );
+  }
+
+  function animate() {
+    var time = clock.getElapsedTime();
+    var min = 1 / config.count;
+
+    var duration = 4;
+    var halfDuration = duration / 2;
+
+    var t = time % duration;
+    geometry = animateGeometry(
+      Math.max( t / halfDuration, min ),
+      Math.max( t / halfDuration, 1 ) - 1 + min
+    );
+
+    createMesh();
+
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+  }
+
+  init();
+  animate();
+
+  window.addEventListener( 'resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  });
+
+})();
---
+++ basic/js/sphere-geometry.js
@@ -0,0 +1,85 @@
+/*global THREE*/
+/*exported createAnimatedSphereGeometry*/
+function createAnimatedSphereGeometry(
+  radius,
+  widthSegments,
+  heightSegments,
+  phiStart,
+  phiLength,
+  thetaStart,
+  thetaLength
+) {
+  'use strict';
+
+  radius = radius || 50;
+
+  widthSegments = Math.max( 3, Math.floor( widthSegments ) || 8 );
+  heightSegments = Math.max( 2, Math.floor( heightSegments ) || 6 );
+
+  phiStart = phiStart !== undefined ? phiStart : 0;
+  phiLength = phiLength !== undefined ? phiLength : Math.PI * 2;
+
+  thetaStart = thetaStart !== undefined ? thetaStart : 0;
+  thetaLength = thetaLength !== undefined ? thetaLength : Math.PI;
+
+  return function( wt, ht ) {
+    wt = THREE.Math.clamp( wt, 0, 1 );
+    ht = THREE.Math.clamp( ht, 0, 1 );
+
+    var geometry = new THREE.Geometry();
+    geometry.boundingSphere = new THREE.Sphere( new THREE.Vector3(), radius );
+
+    var currentWidthSegments = Math.round( wt * widthSegments );
+    var currentHeightSegments = Math.round( ht * heightSegments );
+
+    var x, y;
+    var vertices = [];
+
+    for ( y = 0; y <= currentHeightSegments; y++ ) {
+      var verticesRow = [];
+
+      for ( x = 0; x <= currentWidthSegments; x++ ) {
+        var u = x / widthSegments;
+        var v = y / heightSegments;
+
+        var vertex = new THREE.Vector3();
+        vertex.x = - radius * Math.cos( phiStart + u * phiLength ) * Math.sin( thetaStart + v * thetaLength );
+        vertex.y = radius * Math.cos( thetaStart + v * thetaLength );
+        vertex.z = radius * Math.sin( phiStart + u * phiLength ) * Math.sin( thetaStart + v * thetaLength );
+
+        geometry.vertices.push( vertex );
+
+        verticesRow.push( geometry.vertices.length - 1 );
+      }
+
+      vertices.push( verticesRow );
+    }
+
+    for ( y = 0; y < currentHeightSegments; y++ ) {
+      for ( x = 0; x < currentWidthSegments; x++ ) {
+        var v1 = vertices[ y ][ x + 1 ];
+        var v2 = vertices[ y ][ x ];
+        var v3 = vertices[ y + 1 ][ x ];
+        var v4 = vertices[ y + 1 ][ x + 1 ];
+
+        var n1 = geometry.vertices[ v1 ].clone().normalize();
+        var n2 = geometry.vertices[ v2 ].clone().normalize();
+        var n3 = geometry.vertices[ v3 ].clone().normalize();
+        var n4 = geometry.vertices[ v4 ].clone().normalize();
+
+        if ( Math.abs( geometry.vertices[ v1 ].y ) === radius ) {
+          geometry.faces.push( new THREE.Face3( v1, v3, v4, [ n1, n3, n4 ] ) );
+
+        } else if ( Math.abs( geometry.vertices[ v3 ].y ) === radius ) {
+          geometry.faces.push( new THREE.Face3( v1, v2, v3, [ n1, n2, n3 ] ) );
+
+        } else {
+          geometry.faces.push( new THREE.Face3( v1, v2, v4, [ n1, n2, n4 ] ) );
+          geometry.faces.push( new THREE.Face3( v2, v3, v4, [ n2.clone(), n3, n4.clone() ] ) );
+        }
+      }
+    }
+
+    return geometry;
+  };
+}
