fbfd87681440377d062536d41816055743bbdeba
Raymond Zhou <somecrazyidiot@gmail.com>
2015-06-17 23:25:21 -0400

    uv: Add initial boilerplate.

---
+++ uv/js/main.js
@@ -0,0 +1,33 @@
+/*global THREE*/
+(function() {
+  'use strict';
+
+  var container;
+
+  var scene, camera, controls, renderer;
+
+  function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
+    camera.position.set( 0, 0, 8 );
+    scene.add( camera );
+  }
+
+  function animate() {
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+  }
+
+  init();
+  animate();
+
+})();
