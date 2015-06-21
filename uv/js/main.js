357ea04865b1e632347bee3029eca69a59b7a7fe
Raymond Zhou <somecrazyidiot@gmail.com>
2015-06-20 22:45:27 -0400

    uvs: Add UV map coordinates preview.

--- uv/js/main.js
+++ uv/js/main.js
@@ -4,7 +4,29 @@
 
   var container;
 
+  var canvas, ctx;
+  var size = 256;
+
   var scene, camera, controls, renderer;
+
+  var geometry, material, mesh;
+
+  function drawUvs( ctx, mesh ) {
+    var canvas = ctx.canvas;
+    var width  = canvas.width;
+    var height = canvas.height;
+    ctx.beginPath();
+
+    mesh.geometry.faceVertexUvs[0].forEach(function( uvs ) {
+      ctx.moveTo( width * uvs[0].x, height * uvs[0].y );
+      ctx.lineTo( width * uvs[1].x, height * uvs[1].y );
+      ctx.lineTo( width * uvs[2].x, height * uvs[2].y );
+    });
+
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 0.5;
+    ctx.stroke();
+  }
 
   function init() {
     container = document.createElement( 'div' );
@@ -18,8 +40,37 @@
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
-    camera.position.set( 0, 0, 8 );
+    camera.position.set( 0, 0, 2 );
     scene.add( camera );
+
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+    scene.add( new THREE.HemisphereLight( '#fff', '#000' ) );
+
+    // UV map.
+    canvas = document.createElement( 'canvas' );
+    ctx    = canvas.getContext( '2d' );
+
+    canvas.width  = size;
+    canvas.height = size;
+
+    canvas.style.position = 'fixed';
+    canvas.style.left = 0;
+    canvas.style.top  = 0;
+
+    document.body.appendChild( canvas );
+
+    // Mesh.
+    geometry = new THREE.IcosahedronGeometry( 1, 3 );
+    material = new THREE.MeshPhongMaterial({
+      shininess: 50
+    });
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    ctx.fillStyle = '#fff';
+    ctx.fillRect( 0, 0, size, size );
+    drawUvs( ctx, mesh );
   }
 
   function animate() {
