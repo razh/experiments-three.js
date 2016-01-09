5d6a26bae5d6a7ee170aaf7e23d7ae6c260331aa
Raymond Zhou <somecrazyidiot@gmail.com>
2016-01-08 23:43:35 -0500

    lathe: Add initial boilerplate.

---
+++ lathe/js/main.js
@@ -0,0 +1,24 @@
+/* global THREE */
+(function() {
+  'use strict';
+
+  var container;
+
+  var scene, camera, controls, renderer;
+
+  function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
+  }
+
+  init();
+  renderer.render( scene, camera );
+})();
