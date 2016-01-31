b2a80e9f8f0dd063e8797a4990e6e9bcf9396d08
Raymond Zhou <somecrazyidiot@gmail.com>
2016-01-30 23:59:33 -0500

    skeleton[box]: Add boilerplate for generated box skeleton.

---
+++ skeleton/js/box-main.js
@@ -0,0 +1,81 @@
+/* global THREE, createSkeletonGeometry */
+(function() {
+  'use strict';
+
+  var container;
+
+  var scene, camera, controls, renderer;
+  var geometry, material, mesh;
+
+  var skeletonHelper;
+
+  var clock = new THREE.Clock();
+
+  function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01 );
+    camera.position.set( 0, 0, 2 );
+    scene.add( camera );
+
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+    material = new THREE.MeshBasicMaterial({
+      skinning: true,
+      wireframe: true
+    });
+
+    geometry = createSkeletonGeometry([
+      1.5, 4, 1.5,
+      [
+        [
+          1, 3, 1,
+          [ 0.5, 0.5, 0.5 ]
+        ],
+        [ 1, 2, 1 ]
+      ]
+    ]);
+
+    mesh = new THREE.SkinnedMesh( geometry, material );
+    scene.add( mesh );
+
+    skeletonHelper = new THREE.SkeletonHelper( mesh );
+    skeletonHelper.material.lineWidth = 4;
+    mesh.add( skeletonHelper );
+  }
+
+  function animate() {
+    var time = clock.getElapsedTime();
+    var angle = Math.cos( time );
+
+    mesh.skeleton.bones.forEach(function( bone, index ) {
+      // Set angle if not root.
+      if ( index > 1 ) {
+        bone.rotation.z = angle;
+      }
+    });
+
+    skeletonHelper.update();
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+  }
+
+  init();
+  animate();
+
+  window.addEventListener( 'resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  });
+
+})();
