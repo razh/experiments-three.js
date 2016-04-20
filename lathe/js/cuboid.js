3316565995cc33cd39b03643644a17f37be05ba4
Raymond Zhou <somecrazyidiot@gmail.com>
2016-04-19 23:39:20 -0400

    lathe[cuboid]: Add initial boilerplate.

---
+++ lathe/js/cuboid.js
@@ -0,0 +1,37 @@
+/* global THREE */
+(function() {
+  'use strict';
+
+  var container;
+
+  var scene, camera, renderer;
+
+  function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
+    camera.position.set( 0, 0, 8 );
+  }
+
+  function render() {
+    renderer.render( scene, camera );
+  }
+
+  init();
+  render();
+
+  window.addEventListener( 'resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  });
+
+})();
