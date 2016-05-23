47ae35f48090167a5981302820af99db8f2a513d
Raymond Zhou <somecrazyidiot@gmail.com>
2016-05-22 22:31:24 -0400

    lathe[cuboid]: Add multiple cuboids editor.

--- lathe/js/cuboid-multiple.js
+++ lathe/js/cuboid-multiple.js
@@ -1,4 +1,4 @@
-/* global THREE */
+/* global THREE, createNumericInput, updateGeometry, translateBoxVertices */
 (function() {
   'use strict';
 
@@ -8,11 +8,59 @@
 
   var geometry, material, mesh;
 
+  function setGeometry( _geometry ) {
+    geometry = _geometry;
+    mesh.geometry = geometry;
+    mesh.needsUpdate = true;
+  }
+
+  function createBoxGeometry( dimensions, transformFn, vectors ) {
+    var geometry = new THREE.BoxGeometry( dimensions[0], dimensions[1], dimensions[2] );
+
+    if ( typeof vectors === 'object' ) {
+      translateBoxVertices( geometry, vectors );
+    }
+
+    if ( typeof transformFn === 'function' ) {
+      transformFn( geometry );
+    } else if ( Array.isArray( transformFn ) ) {
+      transformFn.forEach(function( transformFn ) {
+        transformFn( geometry );
+      });
+    }
+
+    return geometry;
+  }
+
+  function mergeGeometries( geometries ) {
+    return geometries.reduce(function( a, b ) {
+      a.merge( b );
+      return a;
+    })
+  }
+
+  function onInput( event ) {
+    try {
+      var fn = new Function( [ 'b' ], event.target.value );
+
+      var _geometries = fn( createBoxGeometry );
+      var _geometry = mergeGeometries( _geometries );
+      updateGeometry( _geometry );
+
+      setGeometry( _geometry );
+      render();
+
+      event.target.setCustomValidity( '' );
+    } catch ( error ) {
+      event.target.setCustomValidity( 'Invalid function' );
+    }
+  }
+
   function init() {
     container = document.createElement( 'div' );
     document.body.appendChild( container );
 
-    renderer = new THREE.WebgGLRenderer({ antialias: true });
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
@@ -23,6 +71,29 @@
 
     var controls = new THREE.OrbitControls( camera, renderer.domElement );
     controls.addEventListener( 'change', render );
+
+    scene.add( new THREE.AmbientLight( '#333' ) );
+
+    var light = new THREE.DirectionalLight();
+    light.position.set( 0, 8, 8 );
+    scene.add( light );
+
+    geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    material = new THREE.MeshStandardMaterial({
+      shading: THREE.FlatShading,
+      transparent: true,
+      opacity: 0.95
+    });
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    var textarea = document.getElementById( 'textarea-commands' );
+    createNumericInput( textarea );
+    textarea.addEventListener( 'input', onInput );
+    // Disable OrbitControls while textarea is focused.
+    textarea.addEventListener( 'keydown', function( event ) {
+      event.stopPropagation();
+    });
   }
 
   function render() {
