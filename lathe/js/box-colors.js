bf32038659afa4a77cdd79c59c52d9a9adb9b1d8
Raymond Zhou <somecrazyidiot@gmail.com>
2016-06-12 22:02:21 -0400

    lathe[cuboid}: Add BoxGeometry vertex colors helper.

--- lathe/js/box-colors.js
+++ lathe/js/box-colors.js
@@ -1,15 +1,55 @@
 /* eslint-env es6 */
-/* global THREE */
+/* global THREE, Indices */
 window.applyBoxVertexColors = (function() {
   'use strict';
 
-  return function vertexColors( geometry ) {
-    const red = new THREE.Color( '#f00');
-    const green = new THREE.Color( '#0f0' );
-    const blue = new THREE.Color( '#00f' );
+  function setFaceVertexColor( face, index, color ) {
+    if ( face.a === index ) {
+      face.vertexColors[ 0 ] = color;
+    }
+
+    if ( face.b === index ) {
+      face.vertexColors[ 1 ] = color;
+    }
+
+    if ( face.c === index ) {
+      face.vertexColors[ 2 ] = color;
+    }
+  }
+
+  return function vertexColors( geometry, colors ) {
+    Object.keys( colors ).forEach( key => {
+      const color = new THREE.Color( colors[ key ] );
+      const indices = Indices[ key.toUpperCase() ];
+
+      geometry.faces.forEach( face => {
+        if ( Array.isArray( indices ) ) {
+          indices.forEach( index =>
+            setFaceVertexColor( face, index, color )
+          );
+        } else {
+          setFaceVertexColor( face, indices, color );
+        }
+      });
+    });
+
+    return geometry;
+  };
+}());
+
+// Like _.defaults, but with THREE.Face vertexColors.
+window.defaultsVertexColor = (function() {
+  'use strict';
+
+  return function defaults( geometry, defaultColor ) {
+    defaultColor = new THREE.Color( defaultColor );
 
     geometry.faces.forEach( face => {
-      face.vertexColors = [ red, green, blue ];
+      for ( let i = 0; i < 3; i++ ) {
+        if ( face.vertexColors[ i ] === undefined ) {
+          face.vertexColors[ i ] = defaultColor;
+        }
+      }
     });
 
     return geometry;
