3030afaa6a21fbb6102f2a4fb26dab4a935a260d
Raymond Zhou <somecrazyidiot@gmail.com>
2016-07-05 23:04:15 -0400

    tactics[ship]: Convert to ES2015.

--- tactics/js/ship/ship.js
+++ tactics/js/ship/ship.js
@@ -1,132 +1,114 @@
+/* eslint-env es6 */
 /*global THREE*/
-/*exported createShipGeometry*/
-var createShipGeometry = (function() {
+/* exported createShipGeometry */
+function createShipGeometry() {
   'use strict';
 
-  function createShipGeometry() {
-    var shape = new THREE.Shape();
+  const shape = new THREE.Shape();
 
-    // Dreadnought-class ships have a length of 160 meters and a beam of 25
-    // meters.
-    var width = 2.5;
-    var length = 16;
+  // Dreadnought-class ships have a length of 160 meters and a beam of 25
+  // meters.
+  const width = 2.5;
+  const length = 16;
 
-    var halfWidth = width / 2;
-    var halfLength = length / 2;
+  const halfWidth = width / 2;
+  const halfLength = length / 2;
 
-    shape.moveTo( 0, halfLength );
+  shape.moveTo( 0, halfLength );
 
-    shape.bezierCurveTo(
-      halfWidth / 3, halfLength,
-      halfWidth, 2 / 3 * halfLength,
-      halfWidth, 0
-    );
+  shape.bezierCurveTo(
+    halfWidth / 3, halfLength,
+    halfWidth, 2 / 3 * halfLength,
+    halfWidth, 0
+  );
 
-    shape.bezierCurveTo(
-      halfWidth, -2 / 3 * halfLength,
-      halfWidth / 3, -halfLength,
-      0, -halfLength
-    );
+  shape.bezierCurveTo(
+    halfWidth, -2 / 3 * halfLength,
+    halfWidth / 3, -halfLength,
+    0, -halfLength
+  );
 
-    shape.bezierCurveTo(
-      -halfWidth / 3, -halfLength,
-      -halfWidth, -2 / 3 * halfLength,
-      -halfWidth, 0
-    );
+  shape.bezierCurveTo(
+    -halfWidth / 3, -halfLength,
+    -halfWidth, -2 / 3 * halfLength,
+    -halfWidth, 0
+  );
 
-    shape.bezierCurveTo(
-      -halfWidth, 2 / 3 * halfLength,
-      -halfWidth / 3, halfLength,
-      0, halfLength
-    );
+  shape.bezierCurveTo(
+    -halfWidth, 2 / 3 * halfLength,
+    -halfWidth / 3, halfLength,
+    0, halfLength
+  );
 
-    return new THREE.ExtrudeGeometry( shape, {
-      amount: 0.35,
-      curveSegments: 32,
-      bevelEnabled: false
-    });
-  }
+  return new THREE.ExtrudeGeometry( shape, {
+    amount: 0.35,
+    curveSegments: 32,
+    bevelEnabled: false
+  });
+}
 
-  return createShipGeometry;
-
-}) ();
-
-/*exported createGunGeometry*/
-var createGunGeometry = (function() {
+/* exported createGunGeometry */
+function createGunGeometry() {
   'use strict';
 
-  function createGunGeometry() {
-    // 14 meter barrel length.
-    var gunLength = 1.4;
-    var gunOffsetX = 0.1;
-    var gunOffsetY = 0.2;
-    var gunRadius = 0.03;
+  // 14 meter barrel length.
+  const gunLength = 1.4;
+  const gunOffsetX = 0.1;
+  const gunOffsetY = 0.2;
+  const gunRadius = 0.03;
 
-    var geometry = new THREE.Geometry();
+  const geometry = new THREE.Geometry();
 
-    var gunLeft = new THREE.CylinderGeometry( gunRadius, gunRadius, gunLength, 16 );
-    var gunRight = gunLeft.clone();
+  const gunLeft = new THREE.CylinderGeometry( gunRadius, gunRadius, gunLength, 16 );
+  const gunRight = gunLeft.clone();
 
-    geometry.merge( gunLeft.translate( -gunOffsetX, gunOffsetY, 0 ) );
-    geometry.merge( gunRight.translate( gunOffsetX, gunOffsetY, 0 ) );
+  geometry.merge( gunLeft.translate( -gunOffsetX, gunOffsetY, 0 ) );
+  geometry.merge( gunRight.translate( gunOffsetX, gunOffsetY, 0 ) );
 
-    return geometry;
+  return geometry;
+}
+
+/* exported createTurretGeometry */
+function createTurretGeometry() {
+  const turretRadius = 0.5;
+  const turretHeight = 0.2;
+
+  const turretShape = new THREE.Shape();
+  turretShape.moveTo( 0, turretRadius );
+
+  const points = [
+    [ 0.3, 1 ],
+    [ 0.6, 0.8 ],
+    [ 0.6, 0 ],
+    [ 0.3, -0.9 ]
+  ];
+
+  for ( let i = 0; i < points.length; i++ ) {
+    const point = points[i];
+    turretShape.lineTo( point[0] * turretRadius, point[1] * turretRadius );
   }
 
-  return createGunGeometry;
-
-}) ();
-
-/*exported createTurretGeometry*/
-var createTurretGeometry = (function() {
-  'use strict';
-
-  function createTurretGeometry() {
-    var turretRadius = 0.5;
-    var turretHeight = 0.2;
-
-    var turretShape = new THREE.Shape();
-    turretShape.moveTo( 0, turretRadius );
-
-    var points = [
-      [ 0.3, 1 ],
-      [ 0.6, 0.8 ],
-      [ 0.6, 0 ],
-      [ 0.3, -0.9 ]
-    ];
-
-    var point;
-    var i, il;
-    for ( i = 0, il = points.length; i < il; i++ ) {
-      point = points[i];
-      turretShape.lineTo( point[0] * turretRadius, point[1] * turretRadius );
-    }
-
-    // Mirror.
-    for ( i = points.length - 1; i >= 0; i-- ) {
-      point = points[i];
-      turretShape.lineTo( -point[0] * turretRadius, point[1] * turretRadius );
-    }
-
-    var geometry = new THREE.ExtrudeGeometry( turretShape, {
-      amount: 0,
-      bevelSize: 0.06,
-      bevelThickness: turretHeight,
-      bevelSegments: 1
-    });
-
-    // Center turret vertically.
-    geometry.translate( 0, 0, -turretHeight / 2 );
-
-    return geometry;
+  // Mirror.
+  for ( let i = points.length - 1; i >= 0; i-- ) {
+    const point = points[i];
+    turretShape.lineTo( -point[0] * turretRadius, point[1] * turretRadius );
   }
 
-  return createTurretGeometry;
+  const geometry = new THREE.ExtrudeGeometry( turretShape, {
+    amount: 0,
+    bevelSize: 0.06,
+    bevelThickness: turretHeight,
+    bevelSegments: 1
+  });
 
-}) ();
+  // Center turret vertically.
+  geometry.translate( 0, 0, -turretHeight / 2 );
 
-/*exported createSmokestackGeometry*/
-var createSmokestackGeometry = (function() {
+  return geometry;
+}
+
+/* exported createSmokestackGeometry */
+function createSmokestackGeometry() {
   'use strict';
 
   /**
@@ -142,65 +124,60 @@ var createSmokestackGeometry = (function() {
    *    \ ____ /    ---
    *
    */
-  function createSmokestackGeometry() {
-    var width = 0.4;
-    var length = 0.35;
+  const width = 0.4;
+  const length = 0.35;
 
-    var halfWidth = width / 2;
-    var halfLength = length / 2;
+  const halfWidth = width / 2;
+  const halfLength = length / 2;
 
-    var shape = new THREE.Shape();
+  const shape = new THREE.Shape();
 
-    // Counter-clockwise starting from top-right.
-    shape.moveTo( halfWidth, -halfLength );
-    // NOTE: Arc API is different from that of canvas.
-    shape.arc(
-      -halfWidth,
-      -(halfLength - halfWidth),
-      halfWidth,
-      Math.PI, 2 * Math.PI
-    );
+  // Counter-clockwise starting from top-right.
+  // NOTE: Arc API is different from that of canvas.
+  shape.moveTo( halfWidth, -halfLength );
+  shape.arc(
+    -halfWidth,
+    -(halfLength - halfWidth),
+    halfWidth,
+    Math.PI, 2 * Math.PI
+  );
 
-    shape.lineTo( -halfWidth, halfLength );
-    shape.arc(
-      halfWidth,
-      halfLength - halfWidth,
-      halfWidth,
-      0, Math.PI
-    );
+  shape.lineTo( -halfWidth, halfLength );
+  shape.arc(
+    halfWidth,
+    halfLength - halfWidth,
+    halfWidth,
+    0, Math.PI
+  );
 
-    // Close path.
-    shape.lineTo( halfWidth, -halfLength );
+  // Close path.
+  shape.lineTo( halfWidth, -halfLength );
 
-    var geometry = new THREE.ExtrudeGeometry( shape, {
-      amount: 1,
-      bevelEnabled: false,
-      curveSegments: 4
-    });
+  const geometry = new THREE.ExtrudeGeometry( shape, {
+    amount: 1,
+    bevelEnabled: false,
+    curveSegments: 4
+  });
 
-    return geometry;
-  }
+  return geometry;
+}
 
-  return createSmokestackGeometry;
-
-}) ();
-
-/*exported createFrontDeckGeometry*/
-var createFrontDeckGeometry = (function() {
+/* exported createFrontDeckGeometry */
+const createFrontDeckGeometry = (function() {
   'use strict';
 
   function splitBezierCurve( v0, v1, v2, v3, t ) {
     // Lerp control points.
-    var v01 = v0.clone().lerp( v1, t );
-    var v12 = v1.clone().lerp( v2, t );
-    var v23 = v2.clone().lerp( v3, t );
+    const v01 = v0.clone().lerp( v1, t );
+    const v12 = v1.clone().lerp( v2, t );
+    const v23 = v2.clone().lerp( v3, t );
 
     // Second iteration.
-    var v012 = v01.clone().lerp( v12, t );
-    var v123 = v12.clone().lerp( v23, t );
+    const v012 = v01.clone().lerp( v12, t );
+    const v123 = v12.clone().lerp( v23, t );
 
-    // Finasl iteration.
-    var v0123 = v012.clone().lerp( v123, t );
+    // Final iteration.
+    const v0123 = v012.clone().lerp( v123, t );
 
     return [
       // First curve, from 0 to t.
@@ -211,36 +188,40 @@ var createFrontDeckGeometry = (function() {
   }
 
   function createFrontDeckGeometry() {
-    var shape = new THREE.Shape();
+    const shape = new THREE.Shape();
 
     // NOTE: These values are the same as the ship geometry.
-    var width = 2.5;
-    var length = 16;
+    const width = 2.5;
+    const length = 16;
 
-    var halfWidth = width / 2;
-    var halfLength = length / 2;
+    const halfWidth = width / 2;
+    const halfLength = length / 2;
 
-    var t = 0.5;
+    const t = 0.5;
 
-    var rightCurve = [
-      new THREE.Vector2( 0, halfLength ),
-      new THREE.Vector2( halfWidth / 3, halfLength ),
-      new THREE.Vector2( halfWidth, 2 / 3 * halfLength ),
-      new THREE.Vector2( halfWidth, 0 ),
+    let rightCurve = [
+      ...[
+        [ 0, halfLength ],
+        [ halfWidth / 3, halfLength ],
+        [ halfWidth, 2 / 3 * halfLength ],
+        [ halfWidth, 0 ]
+      ].map( xy => new THREE.Vector2( ...xy ) ),
       t
     ];
 
-    rightCurve = splitBezierCurve.apply( null, rightCurve )[0];
+    rightCurve = splitBezierCurve( ...rightCurve )[0];
 
-    var leftCurve = [
-      new THREE.Vector2( -halfWidth, 0 ),
-      new THREE.Vector2( -halfWidth, 2 / 3 * halfLength ),
-      new THREE.Vector2( -halfWidth / 3, halfLength ),
-      new THREE.Vector2( 0, halfLength ),
+    let leftCurve = [
+      ...[
+        [ -halfWidth, 0 ],
+        [ -halfWidth, 2 / 3 * halfLength ],
+        [ -halfWidth / 3, halfLength ],
+        [ 0, halfLength ]
+      ].map( xy => new THREE.Vector2( ...xy ) ),
       1 - t
     ];
 
-    leftCurve = splitBezierCurve.apply( null, leftCurve )[1];
+    leftCurve = splitBezierCurve( ...leftCurve )[1];
 
     shape.moveTo( 0, halfLength );
 
@@ -258,7 +239,7 @@ var createFrontDeckGeometry = (function() {
       leftCurve[3].x, leftCurve[3].y
     );
 
-    var geometry = new THREE.ExtrudeGeometry( shape, {
+    const geometry = new THREE.ExtrudeGeometry( shape, {
       amount: 0.2,
       bevelEnabled: false
     });
@@ -267,5 +248,4 @@ var createFrontDeckGeometry = (function() {
   }
 
   return createFrontDeckGeometry;
-
-}) ();
+}());
