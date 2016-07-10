208d98350f92e467ad21fdbb14e928b5abade224
Raymond Zhou <somecrazyidiot@gmail.com>
2016-07-09 23:56:03 -0400

    ref: Add basic template.

--- reference-image/js/reference-image.js
+++ reference-image/js/reference-image.js
@@ -7,5 +7,17 @@ class ReferenceImage extends THREE.Mesh {
   constructor( ...args ) {
     const geometry = new THREE.PlaneBufferGeometry( 1, 1 );
     super( geometry, ...args );
+
+    this.domElement = document.createElement( 'div' );
+    this.domElement.innerHTML = ReferenceImage.template;
   }
 }
+
+ReferenceImage.template = `
+  <form>
+    ${[ 'x', 'y', 'z', 'width', 'height' ].map(key => `
+      <label for="${key}">${key}</label>
+      <input id="${key}" name="${key}" type="number">
+    `).join('')}
+  </form>
+`;
