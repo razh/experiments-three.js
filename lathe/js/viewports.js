1a0a0722e264cc8c2c8324306c396f68b7f63814
Raymond Zhou <somecrazyidiot@gmail.com>
2016-07-23 22:58:02 -0400

    lathe: Add viewports generator.

---
+++ lathe/js/viewports.js
@@ -0,0 +1,32 @@
+/* eslint-env es6 */
+/* global THREE */
+window.createViewports = (function() {
+  'use strict';
+
+  const zero = new THREE.Vector3();
+
+  return function viewports( element, {
+    width = 128,
+    height = 128,
+    distance = 2,
+  } = {} ) {
+    return [ 'x', 'y', 'z' ].reduce( ( views, axis ) => {
+      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setPixelRatio( window.devicePixelRatio );
+      renderer.setSize( width, height );
+      element.appendChild( renderer.domElement );
+
+      const size = 1;
+      const camera = new THREE.OrthographicCamera( -size, size, size, -size );
+      camera.position[ axis ] = distance;
+      camera.lookAt( zero );
+
+      views[ axis ] = {
+        renderer,
+        camera
+      };
+
+      return views;
+    }, {} );
+  };
+}());
