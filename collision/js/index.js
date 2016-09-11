39f01e4980e43b34c62014c6db3e40c865ec2afd
Raymond Zhou <somecrazyidiot@gmail.com>
2016-09-11 15:18:24 -0400

    collision: Add initial boilerplate.

---
+++ collision/js/index.js
@@ -0,0 +1,39 @@
+/* eslint-env es6 */
+/* global THREE */
+
+(function() {
+  'use strict';
+
+  let container;
+
+  let scene, camera, renderer;
+
+  function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
+    camera.position.set( 0, 0, 8 );
+  }
+
+  function render() {
+    renderer.render( scene, camera );
+  }
+
+  init();
+  render();
+
+  window.addEventListener( 'resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    render();
+  });
+}());
