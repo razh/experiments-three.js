fe5f332cc1bf33e377c743bff3360381b6975792
razh <somecrazyidiot@gmail.com>
2016-10-03 23:24:10 -0400

    repl: Add initial boilerplate.

---
+++ repl/js/repl.js
@@ -0,0 +1,37 @@
+/* eslint-env es6 */
+/* global THREE */
+
+(function() {
+  'use strict';
+
+  let container;
+
+  let scene, camera, renderer;
+  let geometry, material, mesh;
+
+  function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight );
+    camera.position.set( 0, 0, 8 );
+
+    const controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.addEventListener( 'change', render );
+
+    scene.add( new THREE.AmbientLight( '#777' ) );
+  }
+
+  function render() {
+    renderer.render( scene, camera );
+  }
+
+  init();
+  render();
+}());
