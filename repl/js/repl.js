cfaa9d27c139011d4f2ee7f0a1f88b99771a7ae1
razh <somecrazyidiot@gmail.com>
2016-10-04 02:00:49 -0400

    repl: Add basic three.js REPL.

--- repl/js/repl.js
+++ repl/js/repl.js
@@ -7,7 +7,12 @@
   let container;
 
   let scene, camera, renderer;
-  let geometry, material, mesh;
+
+  function dispose( mesh ) {
+    if ( mesh instanceof THREE.Mesh ) {
+      mesh.geometry.dispose();
+    }
+  }
 
   function init() {
     container = document.createElement( 'div' );
@@ -25,7 +30,35 @@
     const controls = new THREE.OrbitControls( camera, renderer.domElement );
     controls.addEventListener( 'change', render );
 
-    scene.add( new THREE.AmbientLight( '#777' ) );
+    let group = new THREE.Group();
+    scene.add( group );
+
+    function onInput( event ) {
+      try {
+        group.traverse( dispose );
+        scene.remove( group );
+
+        const fn = new Function( [ 'THREE' ], event.target.value );
+
+        group = new THREE.Group();
+        group.add( ...fn( THREE ) );
+        scene.add( group );
+
+        render();
+
+        if ( !event.target.validity.valid ) {
+          console.info( '' );
+        }
+
+        event.target.setCustomValidity( '' );
+      } catch ( error ) {
+        console.error( error );
+        event.target.setCustomValidity( 'Invalid function' );
+      }
+    }
+
+    const textarea = document.getElementById( 'textarea-commands' );
+    textarea.addEventListener( 'input', onInput );
   }
 
   function render() {
