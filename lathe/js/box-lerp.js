698f56a3ede6152b436c5e064e1c45d57d8dd695
razh <somecrazyidiot@gmail.com>
2016-12-13 02:08:00 -0500

    lathe[cuboid]: Separate lerpBoxVertices() & relativeLerpBoxVertices().

--- lathe/js/box-lerp.js
+++ lathe/js/box-lerp.js
@@ -3,11 +3,29 @@
 window.lerpBoxVertices = (function() {
   'use strict';
 
+  return function lerp( geometry, vertices, vector, t ) {
+    const indices = VertexIndices[ vertices.toUpperCase() ];
+
+    if ( Array.isArray( indices ) ) {
+      indices.forEach( index =>
+        geometry.vertices[ index ].lerp( vector, t )
+      );
+    } else {
+      geometry.vertices[ indices ].lerp( vector, t );
+    }
+
+    return geometry;
+  };
+}());
+
+window.relativeLerpBoxVertices = (function() {
+  'use strict';
+
   const centroidA = new THREE.Vector3();
   const centroidB = new THREE.Vector3();
   const delta = new THREE.Vector3();
 
-  return function lerp( geometryA, verticesA, geometryB, verticesB, t ) {
+  return function relativeLerp( geometryA, verticesA, geometryB, verticesB, t ) {
     const indicesA = VertexIndices[ verticesA.toUpperCase() ];
     const indicesB = VertexIndices[ verticesB.toUpperCase() ];
 
