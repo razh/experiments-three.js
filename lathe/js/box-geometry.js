9edc67b651c5a55dc0c1645e49a695e831313a5b
razh <somecrazyidiot@gmail.com>
2016-12-17 23:56:45 -0500

    lathe[cuboid]: Support string args in BoxGeometry transform helpers.
    In addition to object definitions.

--- lathe/js/box-geometry.js
+++ lathe/js/box-geometry.js
@@ -6,29 +6,39 @@ function transformBoxVertices( method ) {
   const vector = new THREE.Vector3();
   const zero = new THREE.Vector3();
 
+  function baseTransform( geometry, key, delta, ...args ) {
+    const indices = VertexIndices[ key.toUpperCase() ];
+
+    if ( Array.isArray( delta ) ) {
+      vector.fromArray( delta );
+    } else if ( typeof delta === 'object' ) {
+      Object.assign( vector, zero, delta );
+    } else if ( typeof delta === 'number' ) {
+      vector.setScalar( delta );
+    } else {
+      return;
+    }
+
+    if ( Array.isArray( indices ) ) {
+      indices.forEach( index =>
+        geometry.vertices[ index ][ method ]( vector, ...args )
+      );
+    } else {
+      geometry.vertices[ indices ][ method ]( vector, ...args );
+    }
+
+    return geometry;
+  }
+
   return function transform( geometry, vectors, ...args ) {
-    Object.keys( vectors ).forEach( key => {
-      const delta = vectors[ key ];
-      const indices = VertexIndices[ key.toUpperCase() ];
-
-      if ( Array.isArray( delta ) ) {
-        vector.fromArray( delta );
-      } else if ( typeof delta === 'object' ) {
-        Object.assign( vector, zero, delta );
-      } else if ( typeof delta === 'number' ) {
-        vector.setScalar( delta );
-      } else {
-        return;
-      }
-
-      if ( Array.isArray( indices ) ) {
-        indices.forEach( index =>
-          geometry.vertices[ index ][ method ]( vector, ...args )
-        );
-      } else {
-        geometry.vertices[ indices ][ method ]( vector, ...args );
-      }
-    });
+    if ( typeof vectors === 'string' ) {
+      return baseTransform( geometry, vectors, ...args );
+    } else if ( typeof vectors === 'object' ) {
+      Object.keys( vectors ).forEach( key => {
+        const delta = vectors[ key ];
+        baseTransform( geometry, key, delta, ...args );
+      });
+    }
 
     return geometry;
   };
