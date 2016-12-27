e231086c141083b09464c3020da360682c5f0643
razh <somecrazyidiot@gmail.com>
2016-12-27 02:11:19 -0500

    lathe[cuboid]: Add FaceIndices tests. Fix VertexIndices tests.

--- lathe/js/__tests__/box-indices-test.js
+++ lathe/js/__tests__/box-indices-test.js
@@ -12,13 +12,39 @@ describe( 'VertexIndices', () => {
   const getVertexAtIndex = index => box.vertices[ index ];
 
   it( 'matches the orientation of THREE.BoxGeometry vertices', () => {
-    expect( VertexIndices.PX.map( getVertexAtIndex ).every( v => v.x > 0 ) );
-    expect( VertexIndices.NX.map( getVertexAtIndex ).every( v => v.x < 0 ) );
+    expect( VertexIndices.PX.map( getVertexAtIndex ).every( v => v.x > 0 ) ).toBe( true );
+    expect( VertexIndices.NX.map( getVertexAtIndex ).every( v => v.x < 0 ) ).toBe( true );
 
-    expect( VertexIndices.PY.map( getVertexAtIndex ).every( v => v.y > 0 ) );
-    expect( VertexIndices.NY.map( getVertexAtIndex ).every( v => v.y < 0 ) );
+    expect( VertexIndices.PY.map( getVertexAtIndex ).every( v => v.y > 0 ) ).toBe( true );
+    expect( VertexIndices.NY.map( getVertexAtIndex ).every( v => v.y < 0 ) ).toBe( true );
 
-    expect( VertexIndices.PZ.map( getVertexAtIndex ).every( v => v.z > 0 ) );
-    expect( VertexIndices.NZ.map( getVertexAtIndex ).every( v => v.z < 0 ) );
+    expect( VertexIndices.PZ.map( getVertexAtIndex ).every( v => v.z > 0 ) ).toBe( true );
+    expect( VertexIndices.NZ.map( getVertexAtIndex ).every( v => v.z < 0 ) ).toBe( true );
+  });
+});
+
+describe( 'FaceIndices', () => {
+  require('../box-indices');
+  const { FaceIndices } = window;
+
+  const box = new THREE.BoxGeometry( 1, 1, 1 );
+
+  function getVerticesForFaceIndices( faceIndices ) {
+    return faceIndices
+      .map( faceIndex => box.faces[ faceIndex ] )
+      .map( face => [ face.a, face.b, face.c ] )
+      .reduce( ( a, b ) => a.concat( b ) )
+      .map( vertexindex => box.vertices[ vertexindex ] );
+  }
+
+  it( 'matches the orientation of THREE.BoxGeometry faces', () => {
+    expect( getVerticesForFaceIndices( FaceIndices.PX ).every( v => v.x > 0 ) ).toBe( true );
+    expect( getVerticesForFaceIndices( FaceIndices.NX ).every( v => v.x < 0 ) ).toBe( true );
+
+    expect( getVerticesForFaceIndices( FaceIndices.PY ).every( v => v.y > 0 ) ).toBe( true );
+    expect( getVerticesForFaceIndices( FaceIndices.NY ).every( v => v.y < 0 ) ).toBe( true );
+
+    expect( getVerticesForFaceIndices( FaceIndices.PZ ).every( v => v.z > 0 ) ).toBe( true );
+    expect( getVerticesForFaceIndices( FaceIndices.NZ ).every( v => v.z < 0 ) ).toBe( true );
   });
 });
