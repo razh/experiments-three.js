460680abc5245bbaddec63a11c4ef7894333dc91
razh <somecrazyidiot@gmail.com>
2017-01-01 23:17:19 -0500

    repl: Add resize event listener.

--- repl/js/repl.js
+++ repl/js/repl.js
@@ -87,4 +87,12 @@
 
   init();
   render();
+
+  window.addEventListener( 'resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    render();
+  });
 }());
