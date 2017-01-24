22224414fb20c7a93aacd0b21bf048eb43ba653c
razh <somecrazyidiot@gmail.com>
2017-01-23 23:45:33 -0500

    lathe[cuboid]: Improve frame examples.
    - Remove overlap between sides and corners.
    - Add miter version of frame.

--- lathe/js/demos/frame.js
+++ lathe/js/demos/frame.js
@@ -1,32 +1,72 @@
-size = 8
-width = 1
+frame = (size = 8, thickness = 1) => {
+  length = size - (2 * thickness)
+  radius = (size - thickness) / 2
 
-halfWidth = width / 2
-radius = size / 2 - halfWidth
+  dimensions = {
+    x: [thickness, length, thickness],
+    y: [length, thickness, thickness],
+    corner: [thickness, thickness, thickness],
+  }
 
-dimensions = {
-  x: [width, size, width],
-  y: [size, width, width],
-  corner: [width, width, width],
+  const top = _(dimensions.y, t(0, radius, 0))
+  const right = _(dimensions.x, t(radius, 0, 0))
+  const bottom = _(dimensions.y, t(0, -radius, 0))
+  const left = _(dimensions.x, t(-radius, 0, 0))
+
+  const topRight = _(dimensions.corner, t(radius, radius, 0))
+  const bottomRight = _(dimensions.corner, t(radius, -radius, 0))
+  const bottomLeft = _(dimensions.corner, t(-radius, -radius, 0))
+  const topLeft = _(dimensions.corner, t(-radius, radius, 0))
+
+  return [
+    top,
+    right,
+    bottom,
+    left,
+    topRight,
+    bottomRight,
+    bottomLeft,
+    topLeft,
+  ]
 }
 
-const top = _(dimensions.y, t(0, radius, 0))
-const right = _(dimensions.x, t(radius, 0, 0))
-const bottom = _(dimensions.y, t(0, -radius, 0))
-const left = _(dimensions.x, t(-radius, 0, 0))
+frameMiter = (size = 8, thickness = 1) => {
+  radius = (size - thickness) / 2
 
-const topRight = _(dimensions.corner, t(radius, radius, 0))
-const bottomRight = _(dimensions.corner, t(radius, -radius, 0))
-const bottomLeft = _(dimensions.corner, t(-radius, -radius, 0))
-const topLeft = _(dimensions.corner, t(-radius, radius, 0))
+  dimensions = {
+    x: [thickness, size, thickness],
+    y: [size, thickness, thickness],
+  }
+
+  const top = _(dimensions.y, t(0, radius, 0), $t({
+    px_ny: { x: -thickness },
+    nx_ny: { x: thickness },
+  }))
+
+  const right = _(dimensions.x, t(radius, 0, 0), $t({
+    nx_py: { y: -thickness },
+    nx_ny: { y: thickness },
+  }))
+
+  const bottom = _(dimensions.y, t(0, -radius, 0), $t({
+    px_py: { x: -thickness },
+    nx_py: { x: thickness },
+  }))
+
+  const left = _(dimensions.x, t(-radius, 0, 0), $t({
+    px_py: { y: -thickness },
+    px_ny: { y: thickness },
+  }))
+
+  return [
+    top,
+    right,
+    bottom,
+    left,
+  ]
+}
 
 return [
-  top,
-  right,
-  bottom,
-  left,
-  topRight,
-  bottomRight,
-  bottomLeft,
-  topLeft,
+  ...frame().map(t(0, 0, 4)),
+  ...frameMiter().map(t(0, 0, -4)),
 ]
