06ec3af68d96f38c7fda056aa11f2dd3f02b1ffb
razh <somecrazyidiot@gmail.com>
2017-02-01 23:48:49 -0500

    lathe[cuboid]: Add more THREE.BoxGeometry transform examples.

--- lathe/js/demos/examples.js
+++ lathe/js/demos/examples.js
@@ -20,17 +20,50 @@ vertex('px_py_pz', _(d))
 centroid('px')(_(d))
 
 // Setters
+_set = _(d, set('px_py_pz', 8, 8, 8))
 _setX = _(d, setX('px_py_pz', 8))
 _setY = _(d, setY('px_py_pz', 8))
 _setZ = _(d, setZ('px_py_pz', 8))
 
 _copy = _(d, copy('px_py_pz', vertex('px_py_pz')(_setY)))
 
+// Align
+_align = _(d, align('ny'))
+_relativeAlign = (function() {
+  var _d = _(d);
+
+  return $$([
+    _d,
+    _(d, relativeAlign('px')(_d, 'nx')),
+  ])
+}())
+
+// Transforms
+// Rotate
+_rotateX = _(d, rotateX(Math.PI / 4))
+_rotateY = _(d, rotateY(Math.PI / 4))
+_rotateZ = _(d, rotateZ(Math.PI / 4))
+
+// Translate
+_translate = _(d, translate(2, 2, 2))
+
+// Scale
+_scale = _(d, scale(1.5, 1.5, 1.5))
+
+
 return [
+  _set,
   _setX,
   _setY,
   _setZ,
   _copy,
+  _align,
+  _relativeAlign,
+  _rotateX,
+  _rotateY,
+  _rotateZ,
+  _translate,
+  _scale,
 ].map((geometry, index, array) =>
   _(geometry, tz(-16 * (index - ((array.length - 1) / 2))))
 )
