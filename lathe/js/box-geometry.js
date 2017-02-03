e34d5da83df4102e1d8696ec0a9118a56783fbda
razh <somecrazyidiot@gmail.com>
2017-02-02 22:57:07 -0500

    lathe[cuboid]: Support identity THREE.BoxGeometry vertex transforms.

--- lathe/js/box-geometry.js
+++ lathe/js/box-geometry.js
@@ -1,50 +1,52 @@
 /* eslint-env es6 */
 /* global THREE, VertexIndices */
 
-function transformBoxVertices( method ) {
+
+const transformBoxVertices = (function() {
   'use strict';
 
   const vector = new THREE.Vector3();
-  const zero = new THREE.Vector3();
 
-  function baseTransform( geometry, key, delta, ...args ) {
-    const indices = VertexIndices[ key.toUpperCase() ];
+  return ( method, identity = new THREE.Vector3() ) => {
+    function baseTransform( geometry, key, delta, ...args ) {
+      const indices = VertexIndices[ key.toUpperCase() ];
 
-    if ( Array.isArray( delta ) ) {
-      vector.fromArray( delta );
-    } else if ( typeof delta === 'object' ) {
-      Object.assign( vector, zero, delta );
-    } else if ( typeof delta === 'number' ) {
-      vector.setScalar( delta );
-    } else {
+      if ( Array.isArray( delta ) ) {
+        vector.fromArray( delta );
+      } else if ( typeof delta === 'object' ) {
+        Object.assign( vector, identity, delta );
+      } else if ( typeof delta === 'number' ) {
+        vector.setScalar( delta );
+      } else {
+        return geometry;
+      }
+
+      if ( Array.isArray( indices ) ) {
+        indices.forEach( index =>
+          geometry.vertices[ index ][ method ]( vector, ...args )
+        );
+      }
+
       return geometry;
     }
 
-    if ( Array.isArray( indices ) ) {
-      indices.forEach( index =>
-        geometry.vertices[ index ][ method ]( vector, ...args )
-      );
-    }
+    return function transform( geometry, vectors, ...args ) {
+      if ( typeof vectors === 'string' ) {
+        return baseTransform( geometry, vectors, ...args );
+      } else if ( typeof vectors === 'object' ) {
+        Object.keys( vectors ).forEach( key => {
+          const delta = vectors[ key ];
+          baseTransform( geometry, key, delta, ...args );
+        });
+      }
 
-    return geometry;
-  }
-
-  return function transform( geometry, vectors, ...args ) {
-    if ( typeof vectors === 'string' ) {
-      return baseTransform( geometry, vectors, ...args );
-    } else if ( typeof vectors === 'object' ) {
-      Object.keys( vectors ).forEach( key => {
-        const delta = vectors[ key ];
-        baseTransform( geometry, key, delta, ...args );
-      });
-    }
-
-    return geometry;
+      return geometry;
+    };
   };
-}
+}());
 
 window.translateBoxVertices = transformBoxVertices( 'add' );
-window.scaleBoxVertices = transformBoxVertices( 'multiply' );
+window.scaleBoxVertices = transformBoxVertices( 'multiply', new THREE.Vector3( 1, 1, 1 ) );
 window.lerpBoxVertices = transformBoxVertices( 'lerp' );
 
 // Per-axis THREE.BoxGeometry methods.
