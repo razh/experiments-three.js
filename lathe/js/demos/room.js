0f41cb6886f4f3e139c9d4320f0bab78c46da0a5
razh <somecrazyidiot@gmail.com>
2017-02-17 01:39:19 -0500

    lathe[cuboid]: Add atrium 2D miter room example.
    In this version of the atrium, only the vertical walls are mitered.

--- lathe/js/demos/room.js
+++ lathe/js/demos/room.js
@@ -1,4 +1,68 @@
-atrium = (width = 16, height = 8, depth = 16, thickness = 1) => {
+atriumMiter2 = (width = 16, height = 8, depth = 16, thickness = 1) => {
+  rx = (width - thickness) / 2
+  rz = (depth - thickness) / 2
+
+  dimensions = {
+    x: [thickness, height - thickness, depth],
+    y: [width, thickness, depth],
+    z: [width, height - thickness, thickness],
+  }
+
+  right = _(
+    dimensions.x,
+    ty((height + thickness) / 2),
+    tx(rx),
+    $tz({
+      nx_pz: -thickness,
+      nx_nz: thickness,
+    }),
+  )
+
+  left = _(
+    dimensions.x,
+    ty((height + thickness) / 2),
+    tx(-rx),
+    $tz({
+      px_pz: -thickness,
+      px_nz: thickness,
+    }),
+  )
+
+  bottom = _(
+    dimensions.y,
+    ty(thickness / 2),
+  )
+
+  front = _(
+    dimensions.z,
+    ty((height + thickness) / 2),
+    tz(rz),
+    $tx({
+      px_nz: -thickness,
+      nx_nz: thickness,
+    }),
+  )
+
+  back = _(
+    dimensions.z,
+    ty((height + thickness) / 2),
+    tz(-rz),
+    $tx({
+      px_pz: -thickness,
+      nx_pz: thickness,
+    }),
+  )
+
+  return [
+    right,
+    left,
+    bottom,
+    front,
+    back,
+  ]
+}
+
+atriumMiter3 = (width = 16, height = 8, depth = 16, thickness = 1) => {
   rx = (width - thickness) / 2
   rz = (depth - thickness) / 2
 
@@ -10,8 +74,8 @@ atrium = (width = 16, height = 8, depth = 16, thickness = 1) => {
 
   right = _(
     dimensions.x,
-    tx(rx),
     ty(height / 2),
+    tx(rx),
     $ty('nx_ny', thickness),
     $tz({
       nx_pz: -thickness,
@@ -21,8 +85,8 @@ atrium = (width = 16, height = 8, depth = 16, thickness = 1) => {
 
   left = _(
     dimensions.x,
-    tx(-rx),
     ty(height / 2),
+    tx(-rx),
     $ty('px_ny', thickness),
     $tz({
       px_pz: -thickness,
@@ -45,8 +109,8 @@ atrium = (width = 16, height = 8, depth = 16, thickness = 1) => {
 
   front = _(
     dimensions.z,
-    tz(rz),
     ty(height / 2),
+    tz(rz),
     $tx({
       px_nz: -thickness,
       nx_nz: thickness,
@@ -56,8 +120,8 @@ atrium = (width = 16, height = 8, depth = 16, thickness = 1) => {
 
   back = _(
     dimensions.z,
-    tz(-rz),
     ty(height / 2),
+    tz(-rz),
     $tx({
       px_pz: -thickness,
       nx_pz: thickness,
@@ -74,7 +138,7 @@ atrium = (width = 16, height = 8, depth = 16, thickness = 1) => {
   ]
 }
 
-
 return [
-  ...atrium(),
+  ...atriumMiter2().map(tx(12)),
+  ...atriumMiter3().map(tx(-12)),
 ]
