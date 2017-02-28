20fc78d4ce1bf7e8477e96bb43b43468242fb549
razh <somecrazyidiot@gmail.com>
2017-02-27 23:34:32 -0500

    rts: Add game-loop & timestep.

--- rts/js/index.js
+++ rts/js/index.js
@@ -30,9 +30,37 @@
 
     scene.add(plane);
   }
+  const update = (function() {
+    const dt = 1 / 60;
+    let accumulatedTime = 0;
+    let previousTime;
+
+    return () => {
+      const time = (performance.now() || 0) * 1e-3;
+      if (!previousTime) {
+        previousTime = time;
+      }
+
+      const frameTime = Math.min(time - previousTime, 0.1);
+      accumulatedTime += frameTime;
+      previousTime = time;
+
+      while (accumulatedTime >= dt) {
+        scene.traverse(object => {
+          if (typeof object.update === 'function') {
+            object.update(dt);
+          }
+        });
+
+        accumulatedTime -= dt;
+      }
+    };
+  }());
 
   function render() {
+    update();
     renderer.render(scene, camera);
+    requestAnimationFrame(render);
   }
 
   init();
