106499671d780198c902d7b91304dedae5c13139
razh <somecrazyidiot@gmail.com>
2017-03-15 00:47:53 -0400

    lathe[cuboid]: Convert truss example into a function.

--- lathe/js/demos/truss.js
+++ lathe/js/demos/truss.js
@@ -1,58 +1,61 @@
-const thickness = 1
-const outerHeight = 8
-const innerHeight = outerHeight - (2 * thickness)
-const outerWidth = 16
-const innerWidth = outerWidth - (2 * thickness)
+truss = (width = 16, height = 8, thickness = 1) => {
+  const innerWidth = width - (2 * thickness)
+  const innerHeight = height - (2 * thickness)
 
-const dimensions = {
-  x: [outerWidth, thickness, thickness],
-  y: [thickness, innerHeight, thickness],
+  const dimensions = {
+    x: [width, thickness, thickness],
+    y: [thickness, innerHeight, thickness],
+  }
+
+  const horizontalY = (height - thickness) / 2
+  const diagonalX = ((innerWidth - thickness) / 2) - thickness
+  const verticalX = (innerWidth + thickness) / 2
+
+  const top = _(
+    dimensions.x,
+    ty(horizontalY)
+  )
+
+  const bottom = _(
+    dimensions.x,
+    ty(-horizontalY)
+  )
+
+  const vertical = _(dimensions.y)
+
+  const leftDiagonal = _(
+    dimensions.y,
+    tx(-thickness),
+    $tx('py', -diagonalX)
+  )
+
+  const rightDiagonal = _(
+    dimensions.y,
+    tx(thickness),
+    $tx('py', diagonalX)
+  )
+
+  const leftVertical = _(
+    dimensions.y,
+    tx(-verticalX)
+  )
+
+  const rightVertical = _(
+    dimensions.y,
+    tx(verticalX)
+  )
+
+  return [
+    top,
+    bottom,
+    vertical,
+    leftDiagonal,
+    rightDiagonal,
+    leftVertical,
+    rightVertical,
+  ]
 }
 
-const horizontalY = (outerHeight - thickness) / 2
-const diagonalX = ((innerWidth - thickness) / 2) - thickness
-const verticalX = (innerWidth + thickness) / 2
-
-const top = _(
-  dimensions.x,
-  ty(horizontalY)
-)
-
-const bottom = _(
-  dimensions.x,
-  ty(-horizontalY)
-)
-
-const vertical = _(dimensions.y)
-
-const leftDiagonal = _(
-  dimensions.y,
-  tx(-thickness),
-  $tx('py', -diagonalX)
-)
-
-const rightDiagonal = _(
-  dimensions.y,
-  tx(thickness),
-  $tx('py', diagonalX)
-)
-
-const leftVertical = _(
-  dimensions.y,
-  tx(-verticalX)
-)
-
-const rightVertical = _(
-  dimensions.y,
-  tx(verticalX)
-)
-
 return [
-  top,
-  bottom,
-  vertical,
-  leftDiagonal,
-  rightDiagonal,
-  leftVertical,
-  rightVertical,
+  ...truss(),
 ]
