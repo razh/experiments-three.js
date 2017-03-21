02b9a8ed374f13a8ddab9c34e28d365771a8835b
razh <somecrazyidiot@gmail.com>
2017-03-20 22:15:22 -0400

    rts: Add BoxGeometry-based CurvePath.
    Cheap hack in lieu of a Hilbert curve.

---
+++ rts/js/box-curve-path.js
@@ -0,0 +1,16 @@
+/* eslint-env es6 */
+/* global THREE */
+/* exported createBoxCurvePath */
+
+function createBoxCurvePath(...args) {
+  const curvePath = new THREE.CurvePath();
+
+  const { vertices } = new THREE.BoxGeometry(...args);
+  for (let i = 0; i < vertices.length; i++) {
+    const v0 = vertices[i];
+    const v1 = vertices[(i + 1) % vertices.length];
+    curvePath.add(new THREE.LineCurve3(v0.clone(), v1.clone()));
+  }
+
+  return curvePath;
+}
