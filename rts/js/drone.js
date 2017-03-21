2e8aa100002ef49ab319be898eb6160e5c4cbfbf
razh <somecrazyidiot@gmail.com>
2017-03-21 00:22:58 -0400

    rts: Add Drone class.
    Ported from razh/flying-machines.

---
+++ rts/js/drone.js
@@ -0,0 +1,36 @@
+/* eslint-env es6 */
+/* global THREE, Entity */
+/* exported Drone */
+
+class Drone extends Entity {
+  constructor(path) {
+    const geometry = new THREE.IcosahedronBufferGeometry(1);
+    const material = new THREE.MeshPhongMaterial({
+      shading: THREE.FlatShading,
+    });
+
+    super(geometry, material);
+
+    this.path = path;
+    this.time = 0;
+    this.speed = 1;
+  }
+
+  update(dt) {
+    if (!this.path) {
+      return;
+    }
+
+    const duration = this.path.getLength() / this.speed;
+
+    this.time += dt;
+    const t = (this.time / duration) % 1;
+    const point = this.path.getPointAt(t);
+
+    this.velocity
+      .subVectors(point, this.position)
+      .divideScalar(dt);
+
+    this.position.copy(point);
+  }
+}
