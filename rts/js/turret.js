08571d5cc3af7e032aa14edf2a6cc9ffa7bc1910
razh <somecrazyidiot@gmail.com>
2017-03-22 22:46:23 -0400

    rts: Limit pitch/polar-angle rotation of BallTurret.

--- rts/js/turret.js
+++ rts/js/turret.js
@@ -36,5 +36,37 @@ class BallTurret extends THREE.Mesh {
       geometry,
       new THREE.MeshStandardMaterial({ shading: THREE.FlatShading })
     );
+
+    this.offset = new THREE.Vector3();
+    this.spherical = new THREE.Spherical();
+
+    this.minPolarAngle = 0;
+    this.maxPolarAngle = Math.PI / 2;
+  }
+
+  lookAt(vector) {
+    const { offset, spherical } = this;
+
+    // Convert to local position.
+    offset.subVectors(vector, this.position);
+
+    // Convert to spherical coordinates.
+    spherical.setFromVector3(offset);
+
+    // Limit phi.
+    spherical.phi = THREE.Math.clamp(
+      spherical.phi,
+      (Math.PI / 2) - this.maxPolarAngle,
+      (Math.PI / 2) - this.minPolarAngle
+    );
+
+    spherical.makeSafe();
+
+    // Convert to world position.
+    offset
+      .setFromSpherical(spherical)
+      .add(this.position);
+
+    super.lookAt(offset);
   }
 }
