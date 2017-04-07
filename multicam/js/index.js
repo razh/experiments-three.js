bed1dad45499db4991ba8408d5254a30c4fc9e94
razh <somecrazyidiot@gmail.com>
2017-04-06 23:19:25 -0400

    multicam: Convert to ES2015.

--- multicam/js/index.js
+++ multicam/js/index.js
@@ -1,50 +1,51 @@
 /* global THREE */
+
 (function() {
   'use strict';
 
-  var scene;
+  let scene;
 
-  var geometry, material, mesh;
+  let geometry, material, mesh;
 
   // Separate renderers is simpler than scissor testing.
   function createView( selector ) {
-    var container = document.querySelector( selector );
-    var rect = container.getBoundingClientRect();
+    const container = document.querySelector( selector );
+    const rect = container.getBoundingClientRect();
 
-    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( rect.width, rect.height );
     renderer.shadowMap.enabled = true;
     renderer.setClearColor( new THREE.Color().setHSL( 0.6, 0.6, 0.85 ) );
     container.appendChild( renderer.domElement );
 
-    var camera = new THREE.PerspectiveCamera( 60, rect.width / rect.height );
+    const camera = new THREE.PerspectiveCamera( 60, rect.width / rect.height );
 
     return {
-      container: container,
-      renderer: renderer,
-      camera: camera
+      container,
+      renderer,
+      camera,
     };
   }
 
-  var views = {
+  const views = {
     main: createView( '#main' ),
     mounted: createView( '#mounted' ),
     follow: createView( '#follow' )
   };
 
-  var keys = {};
+  const keys = {};
 
   function init() {
     scene = new THREE.Scene();
 
-    var hemiLight = new THREE.HemisphereLight( '#fff', '#fff', 0.5 );
+    const hemiLight = new THREE.HemisphereLight( '#fff', '#fff', 0.5 );
     hemiLight.color.setHSL( 0.6, 1, 0.6 );
     hemiLight.groundColor.setHSL( 0.01, 0.2, 0.8 );
     hemiLight.position.set( 0, 512, 0 );
     scene.add( hemiLight );
 
-    var light = new THREE.DirectionalLight();
+    const light = new THREE.DirectionalLight();
     light.color.setHSL( 0.1, 0.1, 0.95 );
     light.castShadow = true;
     light.shadow.mapSize.set( 1024, 1024 );
@@ -52,20 +53,20 @@
     light.position.set( 8, 32, 32 );
     scene.add( light );
 
-    var planeGeometry = new THREE.PlaneBufferGeometry( 2048, 2048 );
-    var plane = new THREE.Mesh( planeGeometry, new THREE.MeshStandardMaterial({
+    const planeGeometry = new THREE.PlaneBufferGeometry( 2048, 2048 );
+    const plane = new THREE.Mesh( planeGeometry, new THREE.MeshStandardMaterial({
       color: '#343',
       metalness: 0.2,
-      roughness: 0.9
+      roughness: 0.9,
     }));
     plane.rotateX( -Math.PI / 2 );
     plane.receiveShadow = true;
     scene.add( plane );
 
     // Tower.
-    var towerGeometry = new THREE.BoxBufferGeometry( 0.2, 4, 0.2 );
-    var tower = new THREE.Mesh( towerGeometry, new THREE.MeshStandardMaterial({
-      roughness: 0.8
+    const towerGeometry = new THREE.BoxBufferGeometry( 0.2, 4, 0.2 );
+    const tower = new THREE.Mesh( towerGeometry, new THREE.MeshStandardMaterial({
+      roughness: 0.8,
     }));
     tower.position.set( 0, 2, -1.5 );
     tower.castShadow = true;
@@ -73,11 +74,11 @@
     scene.add( tower );
 
     // Launchpad.
-    var launchPadGeometry = new THREE.BoxBufferGeometry( 4, 0.1, 4 );
-    var launchPad = new THREE.Mesh( launchPadGeometry, new THREE.MeshStandardMaterial({
+    const launchPadGeometry = new THREE.BoxBufferGeometry( 4, 0.1, 4 );
+    const launchPad = new THREE.Mesh( launchPadGeometry, new THREE.MeshStandardMaterial({
       color: '#888',
       metalness: 0.1,
-      roughness: 0.9
+      roughness: 0.9,
     }));
     launchPad.castShadow = true;
     launchPad.receiveShadow = true;
@@ -92,17 +93,17 @@
       [ 0.15, 3.5 ],
       [ 0.2, 3.6 ],
       [ 0.2, 4 ],
-      [ 0, 4.2 ]
-    ].reduce(function( array, point ) {
-      var vector = new THREE.Vector2().fromArray( point );
-      // Duplicate to allow for sharp edges (endpoints are deduped).
+      [ 0, 4.2 ],
+    ].reduce(( array, point ) => {
+      const vector = new THREE.Vector2().fromArray( point );
+      // Duplicate to allow for sharp edges (dedupe endpoints).
       array.push( vector, vector );
       return array;
     }, [] ), 32 );
 
     material = new THREE.MeshStandardMaterial({
       metalness: 0.1,
-      roughness: 0.6
+      roughness: 0.6,
     });
 
     mesh = new THREE.Mesh( geometry, material );
@@ -118,25 +119,25 @@
     views.mounted.camera.position.set( -0.3, 2, 0 );
     views.mounted.camera.rotateX( -Math.PI / 2 );
 
-    var cameraHelper = new THREE.CameraHelper( views.mounted.camera );
+    const cameraHelper = new THREE.CameraHelper( views.mounted.camera );
     cameraHelper.visible = false;
     scene.add( cameraHelper );
 
     views.follow.camera.position.set( 8, 0.1, 0 );
   }
 
-  var update = (function() {
-    var clock = new THREE.Clock();
-    var speed = 10;
+  const update = (() => {
+    const clock = new THREE.Clock();
+    const speed = 10;
 
-    return function() {
-      var delta = clock.getDelta();
-      var dy = 0;
+    return () => {
+      const delta = clock.getDelta();
+      let dy = 0;
 
       // W. Up.
-      if ( keys[ 87 ] ) { dy = speed; }
+      if ( keys.KeyW ) { dy += speed; }
       // S. Down.
-      if ( keys[ 83 ] ) { dy = -speed; }
+      if ( keys.KeyS ) { dy -= speed; }
 
       mesh.position.y = Math.max( mesh.position.y + dy * delta, 0 );
       views.follow.camera.lookAt( mesh.position );
@@ -146,8 +147,8 @@
   function animate() {
     update();
 
-    Object.keys( views ).forEach(function( key ) {
-      var view = views[ key ];
+    Object.keys( views ).forEach( key => {
+      const view = views[ key ];
       view.renderer.render( scene, view.camera );
     });
 
@@ -157,24 +158,18 @@
   init();
   animate();
 
-  document.addEventListener( 'keydown', function( event ) {
-    keys[ event.keyCode ] = true;
-  });
+  document.addEventListener( 'keydown', event => keys[ event.code ] = true );
+  document.addEventListener( 'keyup', event => keys[ event.code ] = false );
 
-  document.addEventListener( 'keyup', function( event ) {
-    keys[ event.keyCode ] = false;
-  });
+  window.addEventListener( 'resize', () => {
+    Object.keys( views ).forEach( key => {
+      const view = views[ key ];
 
-  window.addEventListener( 'resize', function() {
-    Object.keys( views ).forEach(function( key ) {
-      var view = views[ key ];
-
-      var rect = view.container.getBoundingClientRect();
+      const rect = view.container.getBoundingClientRect();
       view.camera.aspect = rect.width / rect.height;
       view.camera.updateProjectionMatrix();
 
       view.renderer.setSize( rect.width, rect.height );
     });
   });
-
 })();
