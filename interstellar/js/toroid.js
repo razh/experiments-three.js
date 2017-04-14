adb671bbe07e9dbd9a32bd8a68ceccf0578d466c
Raymond Zhou <somecrazyidiot@gmail.com>
2017-04-13 23:05:39 -0700

    interstellar: Convert Toroid to ES2015 class.

--- interstellar/js/toroid.js
+++ interstellar/js/toroid.js
@@ -1,55 +1,54 @@
-/*global THREE*/
-/*exported Toroid*/
-var Toroid = (function() {
-  'use strict';
+/* eslint comma-dangle: ["error", "always-multiline"] */
+/* global THREE */
+/* exported Toroid */
 
-  /**
-   * This geometry is akin to the inner half of a regular torus with the top
-   * half extended to a circle.
-   *
-   * Cross-section example:
-   *
-   *    --------.        .--------
-   *             \      /
-   *              |    |
-   *             /      \
-   *          --'        '--
-   *
-   * Note the missing outer-half.
-   */
-  function Toroid( outerRadius, innerRadius, tube, radialSegments, tubularSegments, arc ) {
-    THREE.Geometry.call( this );
+/**
+ * This geometry is akin to the inner half of a regular torus with the top
+ * half extended to a circle.
+ *
+ * Cross-section example:
+ *
+ *    --------.        .--------
+ *             \      /
+ *              |    |
+ *             /      \
+ *          --'        '--
+ *
+ * Note the missing outer-half.
+ */
+class Toroid extends THREE.Geometry {
+  constructor(
+    outerRadius = 160,
+    innerRadius = 100,
+    tube = 40,
+    radialSegments = 8,
+    tubularSegments = 6,
+    arc = Math.PI * 2
+  ) {
+    super();
 
     this.parameters = {
-      outerRadius: outerRadius,
-      innerRadius: innerRadius,
-      tube: tube,
-      radialSegments: radialSegments,
-      tubularSegments: tubularSegments,
-      arc: arc
+      outerRadius,
+      innerRadius,
+      tube,
+      radialSegments,
+      tubularSegments,
+      arc,
     };
 
-    outerRadius = outerRadius || 160;
-    innerRadius = innerRadius || 100;
-    tube = tube || 40;
-    radialSegments = radialSegments || 8;
-    tubularSegments = tubularSegments || 6;
-    arc = arc || Math.PI * 2;
+    const center = new THREE.Vector3();
+    const uvs = [];
+    const normals = [];
 
-    var center = new THREE.Vector3();
-    var uvs = [];
-    var normals = [];
-
-    var i, j;
-    for ( j = 0; j <= radialSegments; j++ ) {
-      for ( i = 0; i <= tubularSegments; i++ ) {
-        var u = i / tubularSegments * arc;
-        var v = j / radialSegments * Math.PI + ( 1 / 2 * Math.PI );
+    for ( let j = 0; j <= radialSegments; j++ ) {
+      for ( let i = 0; i <= tubularSegments; i++ ) {
+        const u = i / tubularSegments * arc;
+        const v = j / radialSegments * Math.PI + ( 1 / 2 * Math.PI );
 
         center.x = innerRadius * Math.cos( u );
         center.y = innerRadius * Math.sin( u );
 
-        var vertex = new THREE.Vector3();
+        const vertex = new THREE.Vector3();
         vertex.x = ( innerRadius + tube * Math.cos( v ) ) * Math.cos( u );
         vertex.y = ( innerRadius + tube * Math.cos( v ) ) * Math.sin( u );
         vertex.z = tube * Math.sin( v );
@@ -71,14 +70,14 @@ var Toroid = (function() {
       }
     }
 
-    for ( j = 1; j <= radialSegments; j++ ) {
-      for ( i = 1; i <= tubularSegments; i++ ) {
-        var a = ( tubularSegments + 1 ) * j + i - 1;
-        var b = ( tubularSegments + 1 ) * ( j - 1 ) + i - 1;
-        var c = ( tubularSegments + 1 ) * ( j - 1 ) + i;
-        var d = ( tubularSegments + 1 ) * j + i;
+    for ( let j = 1; j <= radialSegments; j++ ) {
+      for ( let i = 1; i <= tubularSegments; i++ ) {
+        const a = ( tubularSegments + 1 ) * j + i - 1;
+        const b = ( tubularSegments + 1 ) * ( j - 1 ) + i - 1;
+        const c = ( tubularSegments + 1 ) * ( j - 1 ) + i;
+        const d = ( tubularSegments + 1 ) * j + i;
 
-        var face = new THREE.Face3( a, b, d, [ normals[ a ].clone(), normals[ b ].clone(), normals[ d ].clone() ] );
+        let face = new THREE.Face3( a, b, d, [ normals[ a ].clone(), normals[ b ].clone(), normals[ d ].clone() ] );
         this.faces.push( face );
         this.faceVertexUvs[ 0 ].push( [ uvs[ a ].clone(), uvs[ b ].clone(), uvs[ d ].clone() ] );
 
@@ -90,9 +89,4 @@ var Toroid = (function() {
 
     this.computeFaceNormals();
   }
-
-  Toroid.prototype = Object.create( THREE.Geometry.prototype );
-
-  return Toroid;
-
-}) ();
+}
