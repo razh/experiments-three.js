16b1af36245bd0579477f8c982bd5d89a7bb39e8
razh <somecrazyidiot@gmail.com>
2017-04-27 23:42:09 -0400

    rts: Track previous position & velocity in Entity.

--- rts/js/entity.js
+++ rts/js/entity.js
@@ -7,9 +7,15 @@ class Entity extends THREE.Mesh {
 
     this.velocity = new THREE.Vector3();
     this.acceleration = new THREE.Vector3();
+
+    this.previousPosition = new THREE.Vector3();
+    this.previousVelocity = new THREE.Vector3();
   }
 
   update(dt) {
+    this.previousPosition.copy(this.position);
+    this.previousVelocity.copy(this.velocity);
+
     this.velocity.addScaledVector(this.acceleration, dt);
     this.position.addScaledVector(this.velocity, dt);
   }
