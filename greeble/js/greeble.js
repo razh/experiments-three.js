479331c84e495f54734a5e66fbd9d880d5e4966b
razh <somecrazyidiot@gmail.com>
2017-05-07 14:35:06 -0400

    greeble: Convert to ES2015.

--- greeble/js/greeble.js
+++ greeble/js/greeble.js
@@ -1,26 +1,26 @@
-/*global THREE*/
-/*exported greeble*/
-var greeble = (function () {
+/* global THREE */
+/* exported greeble */
+
+const greeble = (() => {
   'use strict';
 
-  var triangle = new THREE.Triangle();
+  const triangle = new THREE.Triangle();
 
   /**
    * Copies of various utility functions from THREE.GeometryUtils.
    *
-   * NOTE: THREE.GeometryUtils resides in the three.js extras folder and is not
-   * included with the bower distribution.
+   * NOTE: THREE.GeometryUtils resides in the three.js extras folder.
    */
   function randomPointInTriangle( vA, vB, vC ) {
-    var a = Math.random();
-    var b = Math.random();
+    let a = Math.random();
+    let b = Math.random();
 
     if ( ( a + b ) > 1 ) {
       a = 1 - a;
       b = 1 - b;
     }
 
-    var c = 1 - a - b;
+    const c = 1 - a - b;
 
     return new THREE.Vector3()
       .addScaledVector( vA, a )
@@ -29,11 +29,9 @@ var greeble = (function () {
   }
 
   function randomPointInFace( face, geometry ) {
-    var vA, vB, vC;
-
-    vA = geometry.vertices[ face.a ];
-    vB = geometry.vertices[ face.b ];
-    vC = geometry.vertices[ face.c ];
+    const vA = geometry.vertices[ face.a ];
+    const vB = geometry.vertices[ face.b ];
+    const vC = geometry.vertices[ face.c ];
 
     return randomPointInTriangle( vA, vB, vC );
   }
@@ -45,20 +43,17 @@ var greeble = (function () {
    * corresponding face normals.
    *
    * @param  {THREE.Geometry} geometry
-   * @param  {Number} n
+   * @param  {Number} count
    * @return {Object}
    */
-  function randomPointsNormalsInGeometry( geometry, n ) {
-    var faces = geometry.faces;
-    var vertices = geometry.vertices;
+  function randomPointsNormalsInGeometry( geometry, count ) {
+    const { faces, vertices } = geometry;
 
-    var totalArea = 0;
-    var cumulativeAreas = [];
+    let totalArea = 0;
+    const cumulativeAreas = [];
 
-    var face;
-    var i, il;
-    for ( i = 0, il = faces.length; i < il; i++ ) {
-      face = faces[i];
+    for ( let i = 0, il = faces.length; i < il; i++ ) {
+      const face = faces[i];
 
       totalArea += triangle
         .setFromPointsAndIndices( vertices, face.a, face.b, face.c )
@@ -73,7 +68,7 @@ var greeble = (function () {
           return start;
         }
 
-        var mid = start + Math.floor( ( end - start ) / 2 );
+        const mid = start + Math.floor( ( end - start ) / 2 );
 
         if ( cumulativeAreas[ mid ] > value ) {
           return binarySearch( start, mid - 1 );
@@ -84,51 +79,40 @@ var greeble = (function () {
         }
       }
 
-      var result = binarySearch( 0, cumulativeAreas.length - 1 );
-      return result;
+      return binarySearch( 0, cumulativeAreas.length - 1 );
     }
 
-    var points = [];
-    var normals = [];
-    var r, index;
-    for ( i = 0; i < n; i++ ) {
-      r = Math.random() * totalArea;
-      index = binarySearchIndices(r);
+    const points = [];
+    const normals = [];
 
-      face = faces[ index ];
-      points[i] = randomPointInFace( face, geometry, true );
+    for ( let i = 0; i < count; i++ ) {
+      const r = Math.random() * totalArea;
+      const index = binarySearchIndices(r);
+
+      const face = faces[ index ];
+      points[i] = randomPointInFace( face, geometry );
       normals[i] = face.normal;
     }
 
-    return {
-      points: points,
-      normals: normals
-    };
+    return { points, normals };
   }
 
+  const matrix = new THREE.Matrix4();
+  const origin = new THREE.Vector3();
+  const up = new THREE.Vector3( 0, 1, 0 );
 
-  var matrix = new THREE.Matrix4();
-  var origin = new THREE.Vector3();
-  var up = new THREE.Vector3( 0, 1, 0 );
+  const emptyGeometry = new THREE.Geometry();
 
-  return function greeble( geometry, options ) {
-    options = options || {};
+  return function greeble( geometry, {
+    count = 0,
+    fn = () => emptyGeometry,
+  } = {} ) {
+    const greebles = new THREE.Geometry();
 
-    var count = options.count;
-    var fn = options.fn;
+    const { points, normals } = randomPointsNormalsInGeometry( geometry, count );
 
-    if ( !count || !fn ) {
-      return;
-    }
-
-    var greebles = new THREE.Geometry();
-
-    var data = randomPointsNormalsInGeometry( geometry, count );
-    var points = data.points;
-    var normals = data.normals;
-
-    points.forEach(function( point, index ) {
-      var normal = normals[ index ];
+    points.forEach(( point, index ) => {
+      const normal = normals[ index ];
 
       // Get orientation and position.
       matrix.identity()
@@ -140,4 +124,4 @@ var greeble = (function () {
 
     return greebles;
   };
-}) ();
+})();
