8e99ba78d37f82c236c01a456e47a1b698e26e87
razh <somecrazyidiot@gmail.com>
2017-05-14 23:36:00 -0400

    bloom: Add Bloom renderer class and example usage.
    Extracted from:
    
    https://github.com/mrdoob/three.js/blob/dev/examples/webgl_postprocessing_unreal_bloom.html

---
+++ bloom/js/bloom.js
@@ -0,0 +1,43 @@
+/* global THREE */
+/* exported Bloom */
+
+class Bloom {
+  constructor(
+    renderer,
+    scene,
+    camera,
+    { strength = 1.5, radius = 0.4, threshold = 0.85 } = {}
+  ) {
+    this.scene = scene;
+    this.camera = camera;
+
+    this.renderPass = new THREE.RenderPass(scene, camera);
+
+    this.fxaa = new THREE.ShaderPass(THREE.FXAAShader);
+    this.fxaa.uniforms.resolution.value.set(
+      1 / window.innerWidth,
+      1 / window.innerHeight
+    );
+
+    this.copy = new THREE.ShaderPass(THREE.CopyShader);
+    this.copy.renderToScreen = true;
+
+    this.bloom = new THREE.UnrealBloomPass(
+      new THREE.Vector2(window.innerWidth, window.innerHeight),
+      strength,
+      radius,
+      threshold
+    );
+
+    this.composer = new THREE.EffectComposer(renderer);
+    this.composer.setSize(window.innerWidth, window.innerHeight);
+    this.composer.addPass(this.renderPass);
+    this.composer.addPass(this.fxaa);
+    this.composer.addPass(this.bloom);
+    this.composer.addPass(this.copy);
+  }
+
+  render() {
+    this.composer.render();
+  }
+}
---
+++ bloom/js/index.js
@@ -0,0 +1,55 @@
+/* global THREE, Bloom */
+
+(() => {
+  'use strict';
+
+  let container;
+
+  let scene;
+  let camera;
+  let renderer;
+
+  let geometry;
+  let material;
+  let mesh;
+
+  let bloom;
+
+  function init() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight);
+    camera.position.set(0, 0, 16);
+
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render);
+
+    const light = new THREE.DirectionalLight();
+    light.position.set(8, 8, 8);
+    scene.add(light);
+
+    geometry = new THREE.IcosahedronBufferGeometry(4, 1);
+    material = new THREE.MeshStandardMaterial({
+      color: 'lightsteelblue',
+      shading: THREE.FlatShading,
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    bloom = new Bloom(renderer, scene, camera);
+  }
+
+  function render() {
+    bloom.render();
+  }
+
+  init();
+  render();
+})();
