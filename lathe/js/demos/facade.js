16ea402c9a24a75dd5d4cd1e881e838d8c5e8c36
razh <somecrazyidiot@gmail.com>
2017-06-08 00:25:33 -0400

    lathe[cuboid]: Convert facade example into a function.

--- lathe/js/demos/facade.js
+++ lathe/js/demos/facade.js
@@ -23,60 +23,64 @@
     - floorHeight: 4
 
   - windowCount: 3
-    - windowWidth: 6
+    - windowWidth: 7
     - windowHeight: 4
     - windowSpacing: 3
  */
 
-const floorCount = 2
-const floorHeight = 4
+facade = ({
+  floorCount = 2,
+  floorHeight = 4,
 
-const windowCount = 3
-const windowWidth = 7
-const windowHeight = 4
-const windowSpacing = 3
+  windowCount = 3,
+  windowWidth = 7,
+  windowHeight = 4,
+  windowSpacing = 3,
 
-const thickness = 1
+  thickness = 1,
+} = {}) => {
+  const geometries = []
 
-const facade = []
-
-const floor = _(
-  [
-    ((windowWidth + windowSpacing) * windowCount) + windowSpacing,
-    floorHeight,
-    thickness,
-  ],
-  align('nx_ny'),
-)
-
-const windowSpacer = _(
-  [windowSpacing, windowHeight, thickness],
-  align('nx_ny'),
-)
-
-for (let y = 0; y < floorCount + 1; y++) {
-  // Floors
-  facade.push(
-    _(
-      floor.clone(),
-      ty((floorHeight + windowHeight) * y),
-    ),
+  const floor = _(
+    [
+      ((windowWidth + windowSpacing) * windowCount) + windowSpacing,
+      floorHeight,
+      thickness,
+    ],
+    align('nx_ny'),
   )
 
-  if (y >= floorCount) {
-    break
-  }
+  const windowSpacer = _(
+    [windowSpacing, windowHeight, thickness],
+    align('nx_ny'),
+  )
 
-  for (let x = 0; x < windowCount + 1; x++) {
-    // Window spacers
-    facade.push(
+  for (let y = 0; y < floorCount + 1; y++) {
+    // Floors
+    geometries.push(
       _(
-        windowSpacer.clone(),
-        tx((windowWidth + windowSpacing) * x),
-        ty(((floorHeight + windowHeight) * y) + floorHeight),
+        floor.clone(),
+        ty((floorHeight + windowHeight) * y),
       ),
     )
+
+    if (y >= floorCount) {
+      break
+    }
+
+    for (let x = 0; x < windowCount + 1; x++) {
+      // Window spacers
+      geometries.push(
+        _(
+          windowSpacer.clone(),
+          tx((windowWidth + windowSpacing) * x),
+          ty(((floorHeight + windowHeight) * y) + floorHeight),
+        ),
+      )
+    }
   }
+
+  return geometries
 }
 
-return facade
+return facade()
