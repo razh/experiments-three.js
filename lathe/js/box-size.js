0c1d1a63d24d547058f3b7e3173cb3842a2ed424
razh <somecrazyidiot@gmail.com>
2017-06-16 01:17:17 -0400

    lathe[cuboid]: Simplify bounding-box size helper functions.

--- lathe/js/box-size.js
+++ lathe/js/box-size.js
@@ -1,29 +1,15 @@
-function sizeBox(geometry) {
+(() => {
   'use strict';
 
-  geometry.computeBoundingBox();
-  return geometry.boundingBox.getSize();
-}
+  function sizeBox(geometry) {
+    geometry.computeBoundingBox();
+    return geometry.boundingBox.getSize();
+  }
 
-function widthBox(geometry) {
-  'use strict';
-
-  return sizeBox(geometry).x;
-}
-
-function heightBox(geometry) {
-  'use strict';
-
-  return sizeBox(geometry).y;
-}
-
-function depthBox(geometry) {
-  'use strict';
-
-  return sizeBox(geometry).z;
-}
-
-window.sizeBox = sizeBox;
-window.widthBox = widthBox;
-window.heightBox = heightBox;
-window.depthBox = depthBox;
+  Object.assign(window, {
+    sizeBox,
+    widthBox: geometry => sizeBox(geometry).x,
+    heightBox: geometry => sizeBox(geometry).y,
+    depthBox: geometry => sizeBox(geometry).z,
+  });
+})();
