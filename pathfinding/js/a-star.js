b4b3b4dbaa53fca4cae012ecda3e062d2a55580a
razh <somecrazyidiot@gmail.com>
2017-09-26 01:09:40 -0400

    pathfinding: Add A* search algorithm.
    https://en.wikipedia.org/wiki/A*_search_algorithm
    https://www.redblobgames.com/pathfinding/a-star/introduction.html
    https://www.redblobgames.com/pathfinding/a-star/implementation.html

---
+++ pathfinding/js/a-star.js
@@ -0,0 +1,57 @@
+/* global minHeap */
+
+'use strict';
+
+function reconstructPath(parents, node) {
+  const path = [];
+
+  while (node) {
+    path.unshift(node);
+    node = parents.get(node);
+  }
+
+  return path;
+}
+
+/* exported aStar */
+function aStar(start, target, nodes, network) {
+  // Distances from start to node.
+  const g = new Map();
+  g.set(start, 0);
+
+  // Distances from node to target.
+  const f = new Map();
+  f.set(start, start.distanceTo(target));
+
+  const closedSet = new Set();
+  const openSet = minHeap((a, b) => f.get(a) - f.get(b));
+  openSet.push(start);
+
+  const parents = new Map();
+
+  while (!openSet.empty()) {
+    const node = openSet.pop();
+    closedSet.add(node);
+
+    if (node === target) {
+      return reconstructPath(parents, node);
+    }
+
+    const neighbors = network[nodes.indexOf(node)].map(index => nodes[index]);
+
+    neighbors.forEach(neighbor => {
+      if (closedSet.has(neighbor)) {
+        return;
+      }
+
+      const distance = g.get(node) + node.distanceTo(neighbor);
+      if (!g.has(neighbor) || distance < g.get(neighbor)) {
+        g.set(neighbor, distance);
+        f.set(neighbor, distance + neighbor.distanceTo(target));
+
+        openSet.push(neighbor);
+        parents.set(neighbor, node);
+      }
+    });
+  }
+}
