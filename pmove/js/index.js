91ab0c76302830633c7c7f99b657576c4a70b43a
Raymond Zhou <somecrazyidiot@gmail.com>
2018-08-25 20:43:20 -0400

    wip

--- pmove/js/index.js
+++ pmove/js/index.js
@@ -1,8 +1,16 @@
+import { Player } from './player.js';
+
 const { THREE } = window;
+
+const keys = {};
 
 let container;
 
 let scene, camera, renderer;
+
+let player, playerMesh;
+
+const clock = new THREE.Clock();
 
 function init() {
   container = document.createElement('div');
@@ -18,28 +26,69 @@ function init() {
     60,
     window.innerWidth / window.innerHeight,
   );
-  camera.position.set(0, 1, 2);
+  camera.position.set(0, 64, 128);
 
   scene.add(new THREE.AmbientLight('#777'));
 
   const light = new THREE.DirectionalLight();
-  light.position.set(8, 8, 8);
+  light.position.set(128, 128, 128);
   scene.add(light);
 
-  scene.add(
-    new THREE.Mesh(
-      new THREE.BoxBufferGeometry(),
-      new THREE.MeshStandardMaterial(),
-    ),
+  // Plane
+  const plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(512, 512),
+    new THREE.MeshStandardMaterial(),
   );
+  plane.rotateX(-Math.PI / 2);
+  scene.add(plane);
+
+  const playerDimensions = new THREE.Vector3(30, 56, 30);
+
+  player = new Player();
+  playerMesh = new THREE.Mesh(
+    new THREE.BoxGeometry(...playerDimensions.toArray()).translate(
+      0,
+      playerDimensions.y / 2,
+      0,
+    ),
+    new THREE.MeshStandardMaterial({ color: '#0f0' }),
+  );
+  scene.add(playerMesh);
+}
+
+function update(dt) {
+  player.command.forwardmove = 0;
+  player.command.rightmove = 0;
+
+  if (keys.KeyW || keys.ArrowUp) player.command.forwardmove += 127;
+  if (keys.KeyS || keys.ArrowDown) player.command.forwardmove -= 127;
+  if (keys.KeyA || keys.ArrowLeft) player.command.rightmove -= 127;
+  if (keys.KeyD || keys.ArrowRight) player.command.rightmove += 127;
+
+  player.viewForward.set(0, 0, -1);
+  player.viewRight.set(1, 0, 0);
+
+  player.frametime = dt;
+  player.update();
+
+  playerMesh.position.copy(player.current.position);
+  camera.position.x = playerMesh.position.x;
+  camera.position.z = playerMesh.position.z;
 }
 
 function render() {
   renderer.render(scene, camera);
 }
 
+function animate() {
+  const dt = clock.getDelta();
+  update(dt);
+  render();
+  requestAnimationFrame(animate);
+}
+
 init();
-render();
+animate();
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -48,3 +97,6 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 });
+
+document.addEventListener('keydown', event => (keys[event.code] = true));
+document.addEventListener('keyup', event => (keys[event.code] = false));
--- pmove/js/player.js
+++ pmove/js/player.js
@@ -1,5 +1,9 @@
 const { THREE } = window;
 
+const STEPSIZE = 18;
+
+const OVERCLIP = 1.001;
+
 // movement parameters
 const PM_STOPSPEED = 100;
 
@@ -8,27 +12,57 @@ const PM_AIRACCELERATE = 1;
 
 const PM_FRICTION = 6;
 
+const g_speed = 320;
+
 class PlayerState {
   constructor() {
-    this.position = new THREE.Vector3(),
-    this.velocity = new THREE.Vector3(),
+    this.position = new THREE.Vector3();
+    this.velocity = new THREE.Vector3();
   }
 }
 
-export default class Player {
+function clipVelocity(vector, normal, overbounce) {
+  let backoff = vector.dot(normal);
+
+  if (backoff < 0) {
+    backoff *= overbounce;
+  } else {
+    backoff /= overbounce;
+  }
+
+  vector.addScaledVector(normal, -backoff);
+}
+
+export class Player {
   constructor() {
     this.current = new PlayerState();
 
+    // player input
+    this.command = {
+      forwardmove: 0,
+      rightmove: 0,
+      upmove: 0,
+    };
+
     // run-time variables
     this.frametime = 0;
+    this.playerSpeed = g_speed;
+    this.viewForward = new THREE.Vector3();
+    this.viewRight = new THREE.Vector3();
 
     // walk movement
-    this.walking = false;
+    // this.walking = false;
+    this.walking = true;
     this.groundPlane = false;
+    this.groundTrace = {
+      plane: {
+        normal: new THREE.Vector3(0, 1, 0),
+      },
+    };
   }
 
   update() {
-    this.groundTrace();
+    this.checkGround();
 
     if (this.walking) {
       // walking on ground
@@ -38,19 +72,113 @@ export default class Player {
       this.airMove();
     }
 
-    this.groundTrace();
+    this.checkGround();
+
+    this.current.position.addScaledVector(
+      this.current.velocity,
+      this.frametime,
+    );
   }
 
   checkJump() {
-
+    if (this.command.upmove < 10) {
+      // not holding jump
+      return false;
+    }
   }
 
   walkMove() {
+    this.friction();
 
+    const fmove = this.command.forwardmove;
+    const smove = this.command.rightmove;
+
+    const cmd = this.command;
+    const scale = this.cmdScale(cmd);
+
+    // project moves down to flat plane
+    this.viewForward.y = 0;
+    this.viewRight.y = 0;
+
+    // project the forward and right directions onto the ground plane
+    clipVelocity(this.viewForward, this.groundTrace.plane.normal, OVERCLIP);
+    clipVelocity(this.viewRight, this.groundTrace.plane.normal, OVERCLIP);
+    //
+    this.viewForward.normalize();
+    this.viewRight.normalize();
+
+    const wishvel = new THREE.Vector3()
+      .addScaledVector(this.viewForward, fmove)
+      .addScaledVector(this.viewRight, smove);
+
+    const wishdir = wishvel.clone();
+    let wishspeed = wishdir.length();
+    wishdir.normalize();
+    wishspeed *= scale;
+
+    this.accelerate(wishdir, wishspeed, PM_ACCELERATE);
+
+    const vel = this.current.velocity.length();
+
+    // slide along the ground plane
+    clipVelocity(
+      this.current.velocity,
+      this.groundTrace.plane.normal,
+      OVERCLIP,
+    );
+
+    // don't decrease velocity when going up or down a slope
+    this.current.velocity.normalize();
+    this.current.velocity.multiplyScalar(vel);
+
+    // don't do anything if standing still
+    if (!this.current.velocity.x && !this.current.velocity.z) {
+      return;
+    }
+
+    this.stepSlideMove(false);
   }
 
   airMove() {
+    this.friction();
 
+    const fmove = this.command.forwardmove;
+    const smove = this.command.rightmove;
+
+    const cmd = this.command;
+    const scale = this.cmdScale(cmd);
+
+    // project moves down to flat plane
+    this.viewForward.y = 0;
+    this.viewRight.y = 0;
+    this.viewForward.normalize();
+    this.viewRight.normalize();
+
+    const wishvel = new THREE.Vector3()
+      .addScaledVector(this.viewForward, fmove)
+      .addScaledVector(this.viewRight, smove);
+    wishvel.y = 0;
+
+    const wishdir = wishvel.clone();
+    let wishspeed = wishdir.length();
+    wishdir.normalize();
+    wishspeed *= scale;
+
+    // not on ground, so little effect on velocity
+    this.accelerate(wishdir, wishspeed, PM_AIRACCELERATE);
+
+    // we may have a ground plane that is very steep, even
+    // though we don't have a groundentity
+    // slide along the steep plane
+    if (this.groundPlane) {
+      clipVelocity(
+        this.current.velocity,
+        this.groundTrace.plane.normal,
+        OVERCLIP,
+      );
+    }
+
+    this.stepSlideMove(true);
   }
 
   friction() {
@@ -61,8 +189,8 @@ export default class Player {
       vec.y = 0; // ignore slope movement
     }
 
-    speed = vec.length();
-    if (speed < 1)  {
+    const speed = vec.length();
+    if (speed < 1) {
       vel.x = 0;
       vel.z = 0; // allow sinking underwater
       // FIXME: still have z friction underwater?
@@ -87,17 +215,48 @@ export default class Player {
     vel.multiplyScalar(newspeed);
   }
 
+  cmdScale() {
+    let max = Math.abs(this.command.forwardmove);
+    if (Math.abs(this.command.rightmove) > max) {
+      max = Math.abs(this.command.rightmove);
+    }
+
+    if (Math.abs(this.command.upmove) > max) {
+      max = Math.abs(this.command.upmove);
+    }
+
+    if (!max) {
+      return 0;
+    }
+
+    const total = Math.sqrt(
+      this.command.forwardmove ** 2 +
+        this.command.rightmove ** 2 +
+        this.command.upmove ** 2,
+    );
+    const scale = (this.playerSpeed * max) / (127 * total);
+
+    return scale;
+  }
+
   accelerate(wishdir, wishspeed, accel) {
     const currentspeed = this.current.velocity.dot(wishdir);
     const addspeed = wishspeed - currentspeed;
     if (addspeed <= 0) {
       return;
     }
-    const accelspeed = accel * this.frametime * wishspeed;
+    let accelspeed = accel * this.frametime * wishspeed;
     if (accelspeed > addspeed) {
       accelspeed = addspeed;
     }
 
     this.current.velocity.addScaledVector(wishdir, accelspeed);
+  }
+
+  checkGround() {}
+
+  stepSlideMove() {
+    const start_o = this.current.position.clone();
+    const start_v = this.current.velocity.clone();
   }
 }
